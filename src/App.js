98f8393d22bf85202e8db70c48a751708a972cec
sCodeu <sachindu99u@gmail.com>
2024-01-14 10:00:49 +0530

    put comment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Add_Customer from "./Pages/Customers/Add_View_Edit/Add_Customer";
 
+
 function App() {
   return (
     <div className="App">
-      <Add_Customer/>
+      <Add_Customer/>{/*create routes for this page */}
     </div>
   );
 }
