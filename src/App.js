20b25ccf9be7aaee099a6a09d19f0270173ae445
sCodeu <sachindu99u@gmail.com>
2024-01-14 10:09:33 +0530

    commented <Add_Customer/> ,creates routes for that

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Add_Customer from "./Pages/Customers/Add_View_Edit/Add_Customer";
 function App() {
   return (
     <div className="App">
-      <Add_Customer/>{/*create routes for this page */}
+      {/*<Add_Customer/>*/}{/*create routes for this page */}
     </div>
   );
 }
