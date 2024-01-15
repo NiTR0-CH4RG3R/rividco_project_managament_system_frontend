b38412e133cdef5b91244480130596525a7e8afc
sCodeu <sachindu99u@gmail.com>
2024-01-15 22:47:18 +0530

    Did some changes in mobile phone validation

--- src/Validation/CustomerValidation.jsx
+++ src/Validation/CustomerValidation.jsx
@@ -1,17 +1,24 @@
 import * as yup from "yup";
 
-export const customerValidation=yup.object().shape({
-   
-    firstName: yup.string().required("First name is required"),
-    lastName:yup.string().required("Last name is required"),
-    address: yup.string().required("Required"),
-    email: yup.string().email("Please enter a valid email").required("Required"),
-    category: yup.string().required("Required"),
-    mobileNumber:yup
+export const customerValidation = yup.object().shape({
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("Required"),
+  address: yup.string().required("Required"),
+  email: yup.string().email("Please enter a valid email").required("Required"),
+  category: yup.string().required("Required"),
+  mobileNumber: yup
     .string()
-    .matches(/^[0-9]{10}$/, "Mobile number must be 10 digits")
+    .matches(
+      /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/,
+      "Please enter a valid mobile number"
+    )
+    .min(10)
     .required("Required"),
-    officeNumber:yup
+  officeNumber: yup
     .string()
-    .matches(/^[0-9]{10}$/, "Mobile number must be 10 digits"),
+    .matches(
+      /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/,
+      "Please enter a valid office number"
+    )
+    .min(10),
 });
