1078e6c394027dc3c4657da90940d9b86b72acbc
sCodeu <sachindu99u@gmail.com>
2024-01-15 15:27:21 +0530

    Did some correction in validation file

--- src/Validation/CustomerValidation.jsx
+++ src/Validation/CustomerValidation.jsx
@@ -1,11 +1,12 @@
 import * as yup from "yup";
 
 export const customerValidation=yup.object().shape({
-    firstName: yup.string().required("Required"),
-    lastName: yup.string().required("Required"),
+   
+    firstName: yup.string().required("First name is required"),
+    lastName:yup.string().required("Last name is required"),
     address: yup.string().required("Required"),
     email: yup.string().email("Please enter a valid email").required("Required"),
-    category: yup.required("Required"),
+    category: yup.string().required("Required"),
     mobileNumber:yup
     .string()
     .matches(/^[0-9]{10}$/, "Mobile number must be 10 digits")
