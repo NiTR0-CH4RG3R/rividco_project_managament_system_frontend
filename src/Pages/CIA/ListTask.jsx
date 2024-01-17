08aee9ee0d14ba893257e14ffde2fdae3145036b
S.M.Jayawardhana <mjayawardhana19240@gmail.com>
2024-01-17 11:12:40 +0530

    Add icon button added and add horizontal scroll bar for data grid

--- src/Pages/CIA/ListTask.jsx
+++ src/Pages/CIA/ListTask.jsx
@@ -1,52 +1,80 @@
-import { Box, Link } from '@mui/material';
+import { AddBoxOutlined } from '@mui/icons-material';
+import { Box, IconButton, Link, Stack } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import * as React from 'react';
 
+//Initialize columns of the data grid
 const columns = [
     {
         field: 'taskId',
         headerName: 'Task Id',
+        align: 'center',
+        headerAlign: 'center'
     },
     {
         field: 'category',
         headerName: 'Category',
+        align: 'center',
+        headerAlign: 'center'
     },
     {
         field: 'requestedBy',
         headerName: 'Requsted By',
+        align: 'center',
+        headerAlign: 'center'
     },
     {
         field: 'addedBy',
         headerName: 'Added By',
+        align: 'center',
+        headerAlign: 'center'
     },
     {
         field: 'addedDate',
         headerName: 'Added Date',
+        align: 'center',
+        headerAlign: 'center',
+        width: '200'
     },
     {
         field: 'assignTo',
         headerName: 'Assign TO',
+        align: 'center',
+        headerAlign: 'center'
     },
     {
         field: 'urgencyLevel',
         headerName: 'Urgency Level',
+        align: 'center',
+        headerAlign: 'center',
+        width: '150'
     },
     {
         field: 'status',
         headerName: 'Status',
+        align: 'center',
+        headerAlign: 'center'
     },
     {
         field: 'projectIdentificationNumber',
         headerName: 'Project Identification Number',
+        align: 'center',
+        headerAlign: 'center',
+        width: '150'
     },
     {
         field: 'callBackNumber',
         headerName: 'Callback Number',
+        align: 'center',
+        headerAlign: 'center',
+        width: '150'
     },
     {
         field: 'chatLink',
         headerName: 'Chat Link',
         width: '200',
+        align: 'center',
+        headerAlign: 'center',
         renderCell: (params) => (
             <Link href={params.value} target="_blank" rel="noopener noreferrer" >
                 {params.value}
@@ -56,10 +84,16 @@ const columns = [
     {
         field: 'description',
         headerName: 'Description',
+        align: 'center',
+        headerAlign: 'center',
+        width: '200'
     },
     {
         field: 'comment',
         headerName: 'Comment',
+        align: 'center',
+        headerAlign: 'center',
+        width: '150'
     },
 ]
 
@@ -99,21 +133,34 @@ function ListTask(){
     const getRowId = (row) => row.taskId;
 
     return(
-        <Box sx={{ height: 400, width: '100%' }}>
-            <DataGrid 
-                columns={columns}
-                rows={task}
-                getRowId={getRowId}
-                initialState={{
-                        pagination: {
-                            paginationModel:{
-                                pageSize: 5,
+        <Box sx={{ height: 500, width: '100%', mt: 10}}>
+
+            <Box sx={{ height: 400 }}>
+                <DataGrid 
+                    columns={columns}
+                    rows={task}
+                    getRowId={getRowId}
+                    initialState={{
+                            pagination: {
+                                paginationModel:{
+                                    pageSize: 5,
+                                },
                             },
-                        },
-                    }}
-                pageSizeOptions={[5]}
-                disableRowSelectionOnClick    
-            /> 
+                        }}
+                    pageSizeOptions={[5, 10]}
+                    disableRowSelectionOnClick
+                    autoWidth = {true}    
+                />
+            </Box>
+            
+            <Box sx={{height: 100, mt: 10, display: 'flex', justifyContent: 'flex-end'}}>
+                <Stack direction="row" spacing={1}>
+                    <IconButton arisl-aria-label='add-button'>
+                        <AddBoxOutlined color='primary' style={{fontSize: 50}}/>
+                    </IconButton>
+                </Stack>
+            </Box>
+
         </Box>
     )
 }
