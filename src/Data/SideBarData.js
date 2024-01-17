42abe1cfc9b4e591a0d64e64251492209d9b2306
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-17 18:28:47 +0530

    Created a configuration file to hold all the paths and components in the project

---
+++ src/Data/SideBarData.js
@@ -0,0 +1,93 @@
+
+import { PersonOutline, Person2Outlined, Shop2Outlined, WorkOutline, HelpCenterOutlined } from "@mui/icons-material";
+
+import Customer from "../Pages/Customer/Customer";
+
+export const menus = [
+    {
+        name: "Customer",
+        icon: <PersonOutline />,
+        subMenu: [
+            {
+                name: "List Customers",
+                path : "/customer/list",
+                component: ( <></> )
+            },
+            {
+                name: "Add Customer",
+                path : "/customer/add",
+                component: <Customer />
+            }
+        ]
+    },
+    {
+        name: "Vendor",
+        icon: <Shop2Outlined />,
+        subMenu: [
+            {
+                name: "List Vendors",
+                path : "/vendor/list",
+            },
+            {
+                name: "Add Vendor",
+                path : "/vendor/add",
+            },
+            {
+                name: "List Vendor Items",
+                path : "/vendor/item/list",
+            },
+            {
+                name: "Add Vendor Item",
+                path : "/vendor/item/add",
+            }
+        ]
+    },
+    {
+        name: "Project",
+        icon: <WorkOutline />,
+        subMenu: [
+            {
+                name: "List Projects",
+                path : "/project/list",
+            },
+            {
+                name: "Add Project",
+                path : "/project/add",
+            },
+            {
+                name: "List Pendnig Services",
+                path : "/project/service/list",
+            }
+        ]
+    },
+
+    {
+        name: "CIA",
+        icon: <HelpCenterOutlined />,
+        subMenu: [
+            {
+                name: "List Tasks",
+                path : "/cia/list",
+            },
+            {
+                name: "Add Task",
+                path : "/cia/add",
+            }
+        ]
+    },
+    
+    {
+        name: "System User",
+        icon: <Person2Outlined />,
+        subMenu: [
+            {
+                name: "List Users",
+                path : "/user/list",
+            },
+            {
+                name: "Add User",
+                path : "/user/add",
+            }
+        ]
+    },
+]
