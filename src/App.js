1fcaff5b6699320dd9ae99a20224c55e8fa042c0
sCodeu <sachindu99u@gmail.com>
2024-01-17 15:11:45 +0530

    Commented Rotes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import Customer from "./Pages/Customer/Customer";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Vendor from "./Pages/Vendor/Vendor";
 
 function App() {
   return (
     <div className="App">
+      {/*
       <Router>
-        
         <Routes>
           <Route path="customer">
-            <Route path="add" element={<Customer type="add"/>}/>
-            <Route path="view/:id" element={<Customer type="view"/>}/>
-            <Route path="update/:id" element={<Customer type="update"/>}/>
+            <Route path="add" element={<Customer type="add" />} />
+            <Route path="view/:id" element={<Customer type="view" />} />
+            <Route path="update/:id" element={<Customer type="update" />} />
           </Route>
           <Route path="vendor">
-          <Route path="add" element={<Vendor type="add"/>}/>
-            <Route path="view/:id" element={<Vendor type="view"/>}/>
-            <Route path="update/:id" element={<Vendor type="update"/>}/>
+            <Route path="add" element={<Vendor type="add" />} />
+            <Route path="view/:id" element={<Vendor type="view" />} />
+            <Route path="update/:id" element={<Vendor type="update" />} />
           </Route>
         </Routes>
-        
-  </Router>
-
+      </Router>
+  */}
     </div>
   );
 }
