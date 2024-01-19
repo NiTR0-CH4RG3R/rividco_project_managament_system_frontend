4ef1b28ea25088b9054a5b6221cfcf40812d4495
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-19 09:44:08 +0530

    Now we create routes from the new AppRoutes file

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,29 @@ import SideBar from "./Components/SideBar/SideBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { menus } from "./Data/SideBarData";
+import { AppRoutes } from "./Data/AppRoutes";
 
 
 export default function App() {
+
+    let routes = [];
+
+    Object.keys(AppRoutes).forEach((key) => { 
+        routes.push( AppRoutes[key] );
+    } )
+
+
     return (
         <Router>
             <Box display='flex' className="App">
                 <SideBar data={menus} />
                 <Routes>
                     {
-                        menus.map((item) => (
-                            item.subMenu.map((subItem) => (
-                                <Route path={subItem.path} element={subItem.component} />
-                            ))
-                        ))
+                        routes.map((route, i) => {
+                            return (
+                                <Route key={i} path={route.path} element={route.component} />
+                            )
+                        })
                     }
                 </Routes>
             </Box>
