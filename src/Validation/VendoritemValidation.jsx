495a37b25302a32e80a2ec65ce68351786ae7931
udeshi <udeshigamage92@gmail.com>
2024-01-19 10:03:45 +0530

    "add vendor item validation"

---
+++ src/Validation/VendoritemValidation.jsx
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+export const VendoritemValidation = yup.object().shape({
+  product_name: yup.string().required("Required"),
+  price: yup.string().required("Required"),
+  warranty_duration: yup.string().required("Required"),
+  capacity: yup.string().required("Required"),
+  vendor: yup.string().required("Required"),
+});
