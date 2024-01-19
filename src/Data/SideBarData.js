b31848c35eccc5a8072dfb5d6d36cb33d5a9a71b
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-19 09:43:40 +0530

    Seperated Routes from SideBarData file. Now all the routes resides in AppRoutes.js file.

--- src/Data/SideBarData.js
+++ src/Data/SideBarData.js
@@ -1,68 +1,34 @@
 
 import { PersonOutline, Person2Outlined, Shop2Outlined, WorkOutline, HelpCenterOutlined } from "@mui/icons-material";
 
-import Customer from "../Pages/Customer/Customer";
-import Vendor from "../Pages/Vendor/Vendor";
-import SystemUser from "../Pages/SystemUser/SystemUser";
-import ListTask from "../Pages/CIA/ListTask";
-import Task from "../Pages/CIA/Task";
+import { AppRoutes as Routes } from "./AppRoutes";
 
 export const menus = [
     {
         name: "Customer",
         icon: <PersonOutline />,
         subMenu: [
-            {
-                name: "List Customers",
-                path : "/customer/list",
-                component: ( <></> )
-            },
-            {
-                name: "Add Customer",
-                path : "/customer/add",
-                component: <Customer type="add" />
-            }
+            { name: "List Customers", path: Routes.customer_list.path, component: Routes.customer_list.component },
+            { name: "Add Customer", path: Routes.customer_add.path, component: Routes.customer_add.component }
         ]
     },
     {
         name: "Vendor",
         icon: <Shop2Outlined />,
         subMenu: [
-            {
-                name: "List Vendors",
-                path : "/vendor/list",
-            },
-            {
-                name: "Add Vendor",
-                path : "/vendor/add",
-                component: <Vendor type="add" />
-            },
-            {
-                name: "List Vendor Items",
-                path : "/vendor/item/list",
-            },
-            {
-                name: "Add Vendor Item",
-                path : "/vendor/item/add",
-            }
+            { name: "List Vendors", path: Routes.vendor_list.path, component: Routes.vendor_list.component },
+            { name: "Add Vendor", path: Routes.vendor_add.path, component: Routes.vendor_add.component },
+            { name: "List Vendor Items", path: Routes.vendor_item_list.path, component: Routes.vendor_item_list.component },
+            { name: "Add Vendor Item", path: Routes.vendor_item_add.path, component: Routes.vendor_item_add.component },
         ]
     },
     {
         name: "Project",
         icon: <WorkOutline />,
         subMenu: [
-            {
-                name: "List Projects",
-                path : "/project/list",
-            },
-            {
-                name: "Add Project",
-                path : "/project/add",
-            },
-            {
-                name: "List Pendnig Services",
-                path : "/project/service/list",
-            }
+            { name: "List Projects", path: Routes.project_list.path, component: Routes.project_list.component },
+            { name: "Add Project", path: Routes.project_add.path, component: Routes.project_add.component },
+            { name: "List Pendnig Services", path: Routes.project_services_list.path, component: Routes.project_services_list.component }
         ]
     },
 
@@ -70,32 +36,17 @@ export const menus = [
         name: "CIA",
         icon: <HelpCenterOutlined />,
         subMenu: [
-            {
-                name: "List Tasks",
-                path : "/cia/list",
-                component: <ListTask />
-            },
-            {
-                name: "Add Task",
-                path : "/cia/add",
-                component: <Task />
-            }
+            { name: "List Tasks", path: Routes.cia_list.path, component: Routes.cia_list.component },
+            { name: "Add Task", path: Routes.cia_add.path, component: Routes.cia_add.component }
         ]
     },
-    
+
     {
         name: "System User",
         icon: <Person2Outlined />,
         subMenu: [
-            {
-                name: "List Users",
-                path : "/user/list",
-            },
-            {
-                name: "Add User",
-                path : "/user/add",
-                component: <SystemUser />
-            }
+            { name: "List Users", path: Routes.system_user_list.path, component: Routes.system_user_list.component },
+            { name: "Add User", path: Routes.system_user_add.path, component: Routes.system_user_add.component }
         ]
     },
 ]
