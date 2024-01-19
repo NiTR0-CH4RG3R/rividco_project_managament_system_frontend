b15dde6ff127683d2349d937d391d42c92e9c600
Nethmi Nuwanthara <rpnnp3107007@gmail.com>
2024-01-19 10:46:01 +0530

    Edit changes => customer list and system user list

---
+++ src/Pages/SystemUser/SystemUserList.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { DataGrid, GridToolbar, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
+import { TextField, Paper, InputAdornment, IconButton } from '@mui/material';
+import SearchRoundedIcon from '@mui/icons-material/Search';
+
+const columns = [
+  { field: 'id', headerName: 'Registration No', width: 150 },
+  { field: 'name', headerName: 'Name', width: 200 },
+  { field: 'profession', headerName: 'Profession', width: 100 },
+  { field: 'address', headerName: 'Address', width: 450 },
+  { field: 'email', headerName: 'E-mail Adress', width: 250 },
+  { field: 'contact1', headerName: 'Contact No1', type: Number, width: 150 },
+];
+
+const rows = [
+  { id:"SC1201", name:"Nethmi Nuwanthara", profession:"Clerk", address:"No 262/E,Ihala biyanwila,Mankada road,Kadawatha", email:"indika456@gmail.com", contact1:760809481},
+  { id:"SC1202", name:"Udeshi Gamage", profession:"Clerk", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"sonali890@gmail.com", contact1:760900481 },
+  { id:"SC1203", name:"Sachindu Umayangana", profession:"Teacher", address:"No 262/E,Pahala biyanwila,Mankada road,Kadawatha", email:"amali92@gmail.com", contact1:760809481 },
+  { id:"SC1204", name:"Buddhima De Soyza", profession:"Teacher", address:"No 300/a,Weliwatta,Palpathwala,Matale", email:"kamal273@gmail.com", contact1:760809481 },
+  { id:"SC1205", name:"Saymi De Alwis", profession:"Lecturer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:760809481},
+  { id:"SC1206", name:"Tharushi Kavindya", profession:"Lecturer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"kamal273@gmail.com", contact1:760900481},
+  { id:"SC1207", name:"Udesh Kumara", profession:"Clerk", address:"No 300/a,Weliwatta,Palpathwala,Matale", email:"sonali890@gmail.com", contact1:714686363},
+  { id:"SC1208", name:"Yashod Gunathilaka", profession:"Lawyer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:766785481},
+  { id:"SC1209", name:"Yashini Perera", profession:"Nurse", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:760809481},
+  { id:"SC1210", name:"Nirmal Gunathilaka", profession:"Doctor", address:"No 300/a,Weliwatta,Palpathwala,Matale", email:"sonali890@gmail.com", contact1:760809481},
+  { id:"SC1211", name:"Aparna Wijerathne", profession:"Lawyer", address:"No 262/E,Ihala biyanwila,Mankada road,Kadawatha", email:"indika456@gmail.com", contact1:760900481},
+  { id:"SC1212", name:"Malithi Perera", profession:"Engineer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:763463784},
+  { id:"SC1213", name:"Kasuni Gunasena", profession:"Nurse", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"amali92@gmail.com", contact1:763463784},
+  { id:"SC1214", name:"Wiharika Niwarthana", profession:"Nurse", address:"No 262/E,Ihala biyanwila,Mankada road,Kadawatha", email:"kamal273@gmail.com", contact1:718182863},
+  { id:"SC1201", name:"Nethmi Nuwanthara", profession:"Clerk", address:"No 262/E,Ihala biyanwila,Mankada road,Kadawatha", email:"indika456@gmail.com", contact1:760809481},
+  { id:"SC1202", name:"Udeshi Gamage", profession:"Clerk", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"sonali890@gmail.com", contact1:760900481 },
+  { id:"gSC1203", name:"Sachindu Umayangana", profession:"Teacher", address:"No 262/E,Pahala biyanwila,Mankada road,Kadawatha", email:"amali92@gmail.com", contact1:760809481},
+  { id:"SC1204", name:"Buddhima De Soyza", profession:"Teacher", address:"No 300/a,Weliwatta,Palpathwala,Matale", email:"kamal273@gmail.com", contact1:760809481},
+  { id:"SC1205", name:"Saymi De Alwis", profession:"Lecturer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:760809481},
+  { id:"SC1206", name:"Tharushi Kavindya", profession:"Lecturer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"kamal273@gmail.com", contact1:760900481},
+  { id:"SC1207", name:"Udesh Kumara", profession:"Clerk", address:"No 300/a,Weliwatta,Palpathwala,Matale", email:"sonali890@gmail.com", contact1:714686363},
+  { id:"SC1208", name:"Yashod Gunathilaka", profession:"Lawyer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:766785481},
+  { id:"SC1209", name:"Yashini Perera", profession:"Nurse", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:760809481},
+  { id:"SC1210", name:"Nirmal Gunathilaka", profession:"Doctor", address:"No 300/a,Weliwatta,Palpathwala,Matale", email:"sonali890@gmail.com", contact1:760809481},
+  { id:"SC1211", name:"Aparna Wijerathne", profession:"Lawyer", address:"No 262/E,Ihala biyanwila,Mankada road,Kadawatha", email:"indika456@gmail.com", contact1:760900481},
+  { id:"SC1212", name:"Malithi Perera", profession:"Engineer", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"indika456@gmail.com", contact1:763463784},
+  { id:"SC1213", name:"Kasuni Gunasena", profession:"Nurse", address:"No 44/18A, 1st cross street,Pagoda road,Nugegoda", email:"amali92@gmail.com", contact1:763463784},
+  { id:"SC1214", name:"Wiharika Niwarthana", profession:"Nurse", address:"No 262/E,Ihala biyanwila,Mankada road,Kadawatha", email:"kamal273@gmail.com", contact1:718182863},
+  
+
+];
+
+const CustomToolbar = ({ onSearchChange }) => {
+  return (
+    <GridToolbarContainer>
+      <TextField
+        variant="standard"
+        reg="search-input"
+        label="Search"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <IconButton>
+               <SearchRoundedIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        onChange={(e) => onSearchChange(e.target.value)}
+      />
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+};
+
+const SytemUser = () => {
+  const [pageSize, setPageSize] = useState(5);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (value) => {
+    setPage(2); 
+    setSearchTerm(value);
+  };
+
+  const filteredRows = rows.filter((row) =>
+    Object.values(row).some(
+      (value) => String(value).toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  );
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <h1><center>System User Datatable</center></h1>
+      <Paper>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={pageSize}
+          page={page}
+          pagination
+          autoHeight
+          components={{
+            Toolbar: () => <CustomToolbar onSearchChange={setSearchTerm} />,
+          }}
+          onPageChange={(params) => setPage(params.page)}
+          onPageSizeChange={(params) => setPageSize(params.pageSize)}
+        />
+      </Paper>
+    </div>
+  );
+};
+
+export default SytemUser;
