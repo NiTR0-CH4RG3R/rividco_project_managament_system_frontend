58b4a7a15b4069b92429214bf990820324f55b38
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-22 10:42:25 +0530

    Added proper routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
-import React from "react";
-import { Box } from '@mui/material';
-import SideBar from "./Components/SideBar/SideBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import { AppRoutes } from './Data/AppRoutes';
 
-import { menus } from "./Data/SideBarData";
-import { AppRoutes } from "./Data/AppRoutes";
 
 
 export default function App() {
 
     let routes = [];
-
-    Object.keys(AppRoutes).forEach((key) => { 
-        routes.push( AppRoutes[key] );
-    } )
-
+    Object.keys(AppRoutes).forEach((name) => {
+        routes.push(AppRoutes[name]);
+    });
 
     return (
-        <Router>
-            <Box display='flex' className="App">
-                <SideBar data={menus} />
-                <Routes>
-                    {
-                        routes.map((route, i) => {
-                            return (
-                                <Route key={i} path={route.path} element={route.component} />
-                            )
-                        })
-                    }
-                </Routes>
-            </Box>
-        </Router>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                {routes.map((route) => (
+                    <Route key={route} path={route.path} element={route.component} />
+                ))}
+
+                {/* <Route path='*' element={<Missing />} /> */}
+            </Route>
+        </Routes>
     );
 }
 
