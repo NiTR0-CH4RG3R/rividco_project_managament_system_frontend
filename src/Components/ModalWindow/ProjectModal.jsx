40af82605d9fba107facc12337a836ae2a778d36
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-01-25 16:12:24 +0530

    Added a project modal with data grid inside the modal

---
+++ src/Components/ModalWindow/ProjectModal.jsx
@@ -0,0 +1,64 @@
+import { Modal } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export default function ProjectModal(props) {
+  const { openProject, setOpenProject, formik } = props
+
+  const handleClose = () => {
+    setOpenProject(false)
+  }
+
+  const [rows, setRows] = useState([])
+
+  useEffect(() => {
+    // Fetch data from REST endpoint using axios
+    axios
+      .get('url to get project data')
+      .then((response) => {
+        setRows(response.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }, [])
+
+  return (
+    <Modal open={openProject} onClose={handleClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          minWidth: 800,
+          minHeight: 400,
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        <Typography variant="h6">Project Modal</Typography>
+        <DataGrid
+          rows={rows}
+          columns={[{ field: 'projectid' }, { field: 'location' }]}
+          disableColumnFilter
+          disableColumnSelector
+          disableDensitySelector
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+            },
+          }}
+          onRowClick={({ row }) => {
+            formik.setFieldValue('selectedProject', row)
+            setOpenProject(false)
+          }}
+        />
+      </Box>
+    </Modal>
+  )
+}
