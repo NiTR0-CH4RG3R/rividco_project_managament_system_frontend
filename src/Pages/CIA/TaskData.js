34b1ca46116b8436a8c58e902d240cdcbcb58881
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-01-25 16:12:50 +0530

    Seperated some task related data from task.jsx file

---
+++ src/Pages/CIA/TaskData.js
@@ -0,0 +1,60 @@
+export const categories = [
+  {
+    value: 'complaint',
+    label: 'Complaint',
+  },
+  {
+    value: 'inquiry',
+    label: 'Inquiry',
+  },
+  {
+    value: 'activity',
+    label: 'Activity',
+  },
+]
+
+export const statuses = [
+  {
+    value: 'active',
+    label: 'Active',
+  },
+  {
+    value: 'on_hold',
+    label: 'On Hold',
+  },
+  {
+    value: 'waiting',
+    label: 'Waiting',
+  },
+  {
+    value: 'rejected',
+    label: 'Rejected',
+  },
+  {
+    value: 'completed',
+    label: 'Completed',
+  },
+]
+
+export const urgencies = [
+  {
+    value: 'critical',
+    label: 'Critical',
+  },
+  {
+    value: 'high_priority',
+    label: 'High Priority',
+  },
+  {
+    value: 'neutral',
+    label: 'Neutral',
+  },
+  {
+    value: 'low_priority',
+    label: 'Low Priority',
+  },
+  {
+    value: 'unknown',
+    label: 'Unknown',
+  },
+]
