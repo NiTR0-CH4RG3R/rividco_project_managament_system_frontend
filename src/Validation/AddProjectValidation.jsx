0a3dbbdae8d90b3b48e66755ba4462f676c6b5c4
sCodeu <sachindu99u@gmail.com>
2024-01-28 13:02:28 +0530

    Added Validation for Project description

--- src/Validation/AddProjectValidation.jsx
+++ src/Validation/AddProjectValidation.jsx
@@ -3,4 +3,6 @@ import * as yup from "yup";
 
 export const addProjectValidation = yup.object().shape({
 
+    selectedCustomer:yup.string().required("Required"),
+    description:yup.string().required("Required"),
 });
