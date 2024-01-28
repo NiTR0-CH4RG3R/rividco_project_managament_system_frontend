fc59dacfffbd8d7052997d85fe75acc7cf2882d0
sCodeu <sachindu99u@gmail.com>
2024-01-28 15:00:51 +0530

    Changed validation selectedCustomer

--- src/Validation/AddProjectValidation.jsx
+++ src/Validation/AddProjectValidation.jsx
@@ -3,7 +3,9 @@ import * as yup from "yup";
 
 export const addProjectValidation = yup.object().shape({
 
-    selectedCustomer:yup.string().required("Required"),
+    selectedCustomer: yup.object({
+        title: yup.string().required("Customer is required"),
+      }),
     description:yup.string().required("Required"),
     status: yup.string().required("Required"),
     location: yup.string().required("Required"),
