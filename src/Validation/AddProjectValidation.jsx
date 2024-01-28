e4389213be1e3b35e5e8b6544c1ed5c0716b4080
sCodeu <sachindu99u@gmail.com>
2024-01-28 16:50:19 +0530

    Added Validation for ReferencedBy

--- src/Validation/AddProjectValidation.jsx
+++ src/Validation/AddProjectValidation.jsx
@@ -1,13 +1,15 @@
 import * as yup from "yup";
 
-
 export const addProjectValidation = yup.object().shape({
+  selectedCustomer: yup.object({
+    title: yup.string().required("Required"),
+  }),
 
-    selectedCustomer: yup.object({
-        title: yup.string().required("Customer is required"),
-      }),
-    description:yup.string().required("Required"),
-    status: yup.string().required("Required"),
-    location: yup.string().required("Required"),
-    startDate:yup.string().required("Required"),
+  selectedReferenceBy: yup.object({
+    title: yup.string().required("Required"),
+  }),
+  description: yup.string().required("Required"),
+  status: yup.string().required("Required"),
+  location: yup.string().required("Required"),
+  startDate: yup.string().required("Required"),
 });
