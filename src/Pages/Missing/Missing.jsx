10b3a61d3de567360908e2c0ea4f363303ce2e50
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-29 12:45:57 +0530

    Created a missing common 404 not found page

---
+++ src/Pages/Missing/Missing.jsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/material/Typography';
+
+export default function Missing() {
+    return (
+        <>
+            <Typography variant="h1"> 404 - Not Found! </Typography>
+        </>
+    )
+}
