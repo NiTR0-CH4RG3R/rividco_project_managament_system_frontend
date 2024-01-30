315a72d82bf3dc6ccc561d2e27cf7474d2ae2fbe
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-30 13:26:22 +0530

    Removed login page route

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 import "./index.css";
-import Login from './Pages/Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
                 <Route path="/*" element={<App />} />
             </Routes>
         </Router>
