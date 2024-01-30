492a3c7fd4eab1b8db374759094fcb4ed3765dce
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-30 12:16:26 +0530

    Added a heading component to the topbar

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { Box, IconButton, AppBar } from '@mui/material';
+import { Box, IconButton, AppBar, Typography } from '@mui/material';
 import { ColorModeContext } from '../../theme';
 import { useContext } from 'react';
 
-import { ArrowBack, Notifications, Brightness4, AccountCircle } from '@mui/icons-material';
+import { ArrowBack, Brightness4, AccountCircle } from '@mui/icons-material';
 
 export default function TopBar({ drawerWidth = 254 }) {
     const colorMode = useContext(ColorModeContext);
@@ -18,17 +18,31 @@ export default function TopBar({ drawerWidth = 254 }) {
                 width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`,
                 display: 'flex',
                 flexDirection: 'row',
-                justifyContent: backButtonVisible ? 'space-between' : 'flex-end',
+                justifyContent: 'space-between',
+                alignItems: 'center',
                 p: 2,
             }}>
 
             {/* Back Button */}
-            <IconButton type='button' sx={{ color: 'grey.50', p: 1, display: (!backButtonVisible && 'none') }}>
-                <ArrowBack />
-            </IconButton>
+            <Box
+                display='flex'
+                flexDirection='column'
+                justifyContent='flex-start'
+            >
+                <Typography variant='h3'>
+                    Primary Heading
+                </Typography>
+
+                <Typography variant='subtitle2'>
+                    Second Heading
+                </Typography>
+            </Box>
 
             {/* Right side icons */}
-            <Box display='flex' alignItems='center'>
+            <Box display='flex' justifyContent='flex-end' alignItems='center'>
+                <IconButton type='button' sx={{ color: 'grey.50', p: 1, display: (!backButtonVisible && 'none') }}>
+                    <ArrowBack />
+                </IconButton>
                 <IconButton type='button' sx={{ color: 'grey.50', p: 1 }} onClick={colorMode.toggleColorMode}>
                     <Brightness4 />
                 </IconButton>
