1248f5b3c6a8f09df1775eeb5d80f75068d7243b
sCodeu <sachindu99u@gmail.com>
2024-02-01 14:07:44 +0530

    Added FormClearButton as a Styled Components

---
+++ src/Components/StyledComponents/FormSaveLoadingButton.jsx
@@ -0,0 +1,7 @@
+import { styled, alpha } from "@mui/system";
+import { LoadingButton } from "@mui/lab";
+
+const FormSaveLoadingButton = styled(LoadingButton)(({ theme }) => ({
+  //add styles here
+}));
+export default FormSaveLoadingButton;
