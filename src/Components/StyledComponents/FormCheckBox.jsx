a3c58049053e058893ec5ac2ea2f6a65effceb9b
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-01 14:31:42 +0530

    Created the structure of the styled component Form Check Box

---
+++ src/Components/StyledComponents/FormCheckBox.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { CheckBox } from '@mui/icons-material'
+
+const FormCheckBox = styled(CheckBox)(({ theme }) => ({
+  //style form checkbox here
+}))
+
+export default FormCheckBox
