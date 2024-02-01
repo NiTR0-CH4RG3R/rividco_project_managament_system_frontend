97162ef0017f7aa6d385301e5c57578fd2d76cb1
sCodeu <sachindu99u@gmail.com>
2024-02-01 13:53:16 +0530

    Added styled textfield

---
+++ src/Components/StyledComponents/FormTextField.jsx
@@ -0,0 +1,7 @@
+import { styled, alpha } from "@mui/system";
+import { TextField } from "@mui/material";
+
+const FormTextField = styled(TextField)(({ theme }) => ({
+  //add styles here
+}));
+export default FormTextField;
