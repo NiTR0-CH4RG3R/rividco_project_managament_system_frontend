7d68b547c112578e019f79eeb6a6741f83cbabd8
sCodeu <sachindu99u@gmail.com>
2024-02-01 14:18:46 +0530

    Added FormButton as a StyledComponents

---
+++ src/Components/StyledComponents/FormButton.jsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+import { styled, alpha } from "@mui/system";
+
+
+const FormButton = styled(Button)(({ theme }) => ({
+  //add styles here
+}));
+export default FormButton;
