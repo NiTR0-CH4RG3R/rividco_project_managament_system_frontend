98d880fad0c228a61ede4a942cd757a382b882d0
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-01 14:29:47 +0530

    Created the structure of TopBarIconButton

---
+++ src/Components/StyledComponents/TopBarIconButton.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { IconButton } from '@mui/material'
+
+const TopBarIconButton = styled(IconButton)(({ theme }) => ({
+  //style form TopBarIconButton here
+}))
+
+export default TopBarIconButton
