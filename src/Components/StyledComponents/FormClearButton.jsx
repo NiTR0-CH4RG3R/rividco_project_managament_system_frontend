cacc2f7ea0049c006626646a73626dfc6922e64c
sCodeu <sachindu99u@gmail.com>
2024-02-01 14:08:01 +0530

    Added FormClearButton as a Styled Components

---
+++ src/Components/StyledComponents/FormClearButton.jsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+import { styled, alpha } from "@mui/system";
+
+
+const FormClearButton = styled(Button)(({ theme }) => ({
+  //add styles here
+}));
+export default FormClearButton;
