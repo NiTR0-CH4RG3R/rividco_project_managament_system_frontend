d01e98403c298a4107586847f84bc07c0aef3f2b
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-01 14:32:14 +0530

    Created the structure of the styled component Form Edit Button

---
+++ src/Components/StyledComponents/FormEditButton.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { Button } from '@mui/material'
+
+const FormEditButton = styled(Button)(({ theme }) => ({
+  //style form edit button here
+}))
+
+export default FormEditButton
