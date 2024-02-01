afb19522c2500a26b252537f36f8f3a0a3551f6d
sCodeu <sachindu99u@gmail.com>
2024-02-01 14:30:32 +0530

    Added ListTable as Styled Component

---
+++ src/Components/StyledComponents/ListTable.jsx
@@ -0,0 +1,8 @@
+import { Table } from "@mui/material";
+import { styled, alpha } from "@mui/system";
+
+
+const ListTable = styled(Table)(({ theme }) => ({
+  //add styles here
+}));
+export default ListTable;
