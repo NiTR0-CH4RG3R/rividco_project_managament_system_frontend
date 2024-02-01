d54bed33de929b85950ae9275b9c70331353f6c6
sCodeu <sachindu99u@gmail.com>
2024-02-01 14:21:26 +0530

    Added FormTypography As a styled Component

---
+++ src/Components/StyledComponents/FormTypography.jsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import { styled, alpha } from "@mui/system";
+
+
+const FormTypography = styled(Typography)(({ theme }) => ({
+  //add styles here
+}));
+export default FormTypography;
