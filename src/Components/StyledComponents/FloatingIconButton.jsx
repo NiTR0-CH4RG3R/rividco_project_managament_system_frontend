132fb8a62b3245aa41a85bc7136f8adf6498ca2e
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-01 14:31:12 +0530

    Created the structure of the styled component Floating Icon Button

---
+++ src/Components/StyledComponents/FloatingIconButton.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { IconButton } from '@mui/material'
+
+const FloatingIconButton = styled(IconButton)(({ theme }) => ({
+  //style form Floating Icon Button  here
+}))
+
+export default FloatingIconButton
