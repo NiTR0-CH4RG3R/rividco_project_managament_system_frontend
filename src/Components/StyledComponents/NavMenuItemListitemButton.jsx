730ec245196d5900105b374679fde75e6da1c965
sCodeu <sachindu99u@gmail.com>
2024-02-01 14:35:11 +0530

    Added NavMenuitemListitemButton as style components

---
+++ src/Components/StyledComponents/NavMenuItemListitemButton.jsx
@@ -0,0 +1,8 @@
+import { ListItemButton } from "@mui/material";
+import { styled, alpha } from "@mui/system";
+
+
+const NavMenuitemListitemButton = styled(ListItemButton)(({ theme }) => ({
+  //add styles here
+}));
+export default NavMenuitemListitemButton;
