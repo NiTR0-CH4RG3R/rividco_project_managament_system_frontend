36487dc17f579436440fdb7e1140f956d4870410
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-01 14:33:45 +0530

    Created the structure of the styled component NavSubItemListItemButton

---
+++ src/Components/StyledComponents/NavSubItemListItemButton.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { ListItemButton } from '@mui/material'
+
+const NavSubItemListItemButton = styled(ListItemButton)(({ theme }) => ({
+  //style form Nav Sub Item ListItemButton here
+}))
+
+export default NavSubItemListItemButton
