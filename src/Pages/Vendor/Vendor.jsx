789d658bc113d3586bca96d04e7d76e50afac7fa
sCodeu <sachindu99u@gmail.com>
2024-02-03 21:20:48 +0530

    Remove unnessary dev tag

--- src/Pages/Vendor/Vendor.jsx
+++ src/Pages/Vendor/Vendor.jsx
@@ -248,13 +248,7 @@ export default function Vendor(props) {
           </>
         )}
         {props.type === "view" && (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "end",
-              padding: "1em 2em 0em 2em !important",
-            }}
-          >
+          
             <Button
               variant="contained"
               sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
@@ -264,7 +258,7 @@ export default function Vendor(props) {
             >
               Edit
             </Button>
-          </div>
+          
         )}
       </Box>
     </Box>
