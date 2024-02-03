3366e9c3bca793d07afd4783aa2599936dbf53c8
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-04 01:59:13 +0530

    Added styled components to task resource page

--- src/Pages/CIA/AddTaskResources.jsx
+++ src/Pages/CIA/AddTaskResources.jsx
@@ -1,126 +1,130 @@
-import React, { useEffect } from "react";
-import { Button, TextField, Grid, MenuItem, Box } from "@mui/material";
-import ClearAllIcon from "@mui/icons-material/ClearAll";
-import SaveIcon from "@mui/icons-material/Save";
-import { useFormik } from "formik";
-import TaskResourcesValidation from "../../Validation/TaskResourcesValidation";
-import { useNavigate, useParams } from "react-router-dom";
-import { AddBox, FileUpload, Visibility, WhatsApp } from "@mui/icons-material";
-import { useTopbarContext } from "../../Contexts/TopbarContext";
+import React, { useEffect } from 'react'
+import { Button, TextField, Grid, MenuItem, Box } from '@mui/material'
+import ClearAllIcon from '@mui/icons-material/ClearAll'
+import SaveIcon from '@mui/icons-material/Save'
+import { useFormik } from 'formik'
+import TaskResourcesValidation from '../../Validation/TaskResourcesValidation'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AddBox, FileUpload, Visibility, WhatsApp } from '@mui/icons-material'
+import { useTopbarContext } from '../../Contexts/TopbarContext'
+import FormTextField from '../../Components/StyledComponents/FormTextField'
+import FormButton from '../../Components/StyledComponents/FormButton'
+import FormClearButton from '../../Components/StyledComponents/FormClearButton'
+import FormSaveLoadingButton from '../../Components/StyledComponents/FormSaveLoadingButton'
 
 const uploadFile = async (file, category) => {
   // Replace this with actual file storage
-  console.log(`Uploading file to category ${category}:`, file.name);
+  console.log(`Uploading file to category ${category}:`, file.name)
 
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log(`File uploaded successfully to category ${category}.`);
-      resolve({ success: true });
-    }, 2000);
-  });
-};
+      console.log(`File uploaded successfully to category ${category}.`)
+      resolve({ success: true })
+    }, 2000)
+  })
+}
 
 const AddTaskResources = (props) => {
-  const { setTitle, setSubtitle } = useTopbarContext();
+  const { setTitle, setSubtitle } = useTopbarContext()
   setTitle(
-    props.type === "add"
-      ? "Add a new CIA Task Resource"
+    props.type === 'add'
+      ? 'Add a new CIA Task Resource'
       : `View CIA Task Resource`
-  );
+  )
   setSubtitle(
-    props.type === "add"
-      ? "You can add CIA Task Resource here."
+    props.type === 'add'
+      ? 'You can add CIA Task Resource here.'
       : `You can CIA Task Resource details here.`
-  );
+  )
   const formik = useFormik({
     initialValues: {
-      taskId: "",
-      selectedPath: "",
+      taskId: '',
+      selectedPath: '',
       selectedFile: null,
-      addedBy: "",
-      addedDate: "",
-      comment: "",
+      addedBy: '',
+      addedDate: '',
+      comment: '',
     },
     validationSchema: TaskResourcesValidation,
     onSubmit: async (values) => {
       try {
-        await TaskResourcesValidation.validate(values, { abortEarly: false });
+        await TaskResourcesValidation.validate(values, { abortEarly: false })
 
         // Add form submission logic here
-        console.log("Form submitted with values:", values);
+        console.log('Form submitted with values:', values)
       } catch (error) {
-        console.error("Validation Error:", error.message);
+        console.error('Validation Error:', error.message)
       }
     },
-  });
+  })
 
   //TaskID get from the db
   useEffect(() => {
     const fetchTaskId = async () => {
       try {
-        const response = await fetch("http://localhost:3001/taskId");
-        const data = await response.json();
-        formik.setFieldValue("taskId", data.taskId);
+        const response = await fetch('http://localhost:3001/taskId')
+        const data = await response.json()
+        formik.setFieldValue('taskId', data.taskId)
       } catch (error) {
-        console.error("Error fetching TaskId:", error.message);
+        console.error('Error fetching TaskId:', error.message)
       }
-    };
+    }
 
-    fetchTaskId();
-  }, [formik]);
+    fetchTaskId()
+  }, [formik])
 
   //File and file path changing
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    formik.setFieldValue("selectedFile", file);
-  };
+    const file = event.target.files[0]
+    formik.setFieldValue('selectedFile', file)
+  }
 
   const handlePathChange = (event) => {
-    const path = event.target.value;
-    formik.setFieldValue("selectedPath", path);
-  };
+    const path = event.target.value
+    formik.setFieldValue('selectedPath', path)
+  }
 
   //upload file
   const handleUpload = async () => {
-    const { selectedFile, selectedPath } = formik.values;
+    const { selectedFile, selectedPath } = formik.values
 
     try {
       if (!selectedFile) {
-        alert("Please select a file.");
-        return;
+        alert('Please select a file.')
+        return
       }
 
-      const uploadResponse = await uploadFile(selectedFile, selectedPath);
+      const uploadResponse = await uploadFile(selectedFile, selectedPath)
 
       if (uploadResponse.success) {
-        console.log("File uploaded successfully.");
+        console.log('File uploaded successfully.')
       } else {
-        console.error("File upload failed.");
+        console.error('File upload failed.')
       }
 
-      formik.setFieldValue("selectedFile", null);
+      formik.setFieldValue('selectedFile', null)
     } catch (error) {
-      console.error("Error uploading file:", error.message);
+      console.error('Error uploading file:', error.message)
     }
-  };
+  }
 
   const handleClear = () => {
-    formik.resetForm();
-  };
+    formik.resetForm()
+  }
 
   const openWhatsApp = () => {
-    window.open("https://web.whatsapp.com/", "_blank");
-  };
+    window.open('https://web.whatsapp.com/', '_blank')
+  }
 
   const openDrive = () => {
     window.open(
-      "https://drive.google.com/drive/folders/1UAwL8lAT7AZ-75HvAepfpEePNZoHMw-b?usp=sharing",
-      "_blank"
-    );
-  };
+      'https://drive.google.com/drive/folders/1UAwL8lAT7AZ-75HvAepfpEePNZoHMw-b?usp=sharing',
+      '_blank'
+    )
+  }
 
-  const { id } = useParams();
-  const navi = useNavigate();
+  const { id } = useParams()
+  const navi = useNavigate()
 
   return (
     <div>
@@ -128,7 +132,7 @@ const AddTaskResources = (props) => {
         <Box
           component="form"
           sx={{
-            "& .MuiTextField-root": { m: 1 },
+            '& .MuiTextField-root': { m: 1 },
           }}
           noValidate
           autoComplete="off"
@@ -138,73 +142,73 @@ const AddTaskResources = (props) => {
           height="100vh"
           flexDirection="column"
         >
-          <Grid container spacing={2} sx={{ width: "70%" }}>
+          <Grid container spacing={2} sx={{ width: '70%' }}>
             {/* ---------------TaskID Field---------------- */}
 
-            <Grid item xs={10} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <TextField
+            <Grid item xs={10} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormTextField
                 id="taskId"
                 variant="outlined"
                 disabled
                 label="TaskId"
-                sx={{ width: "100%" }}
-                {...formik.getFieldProps("taskId")}
+                sx={{ width: '100%' }}
+                {...formik.getFieldProps('taskId')}
               />
             </Grid>
 
             {/* ---------------Whatsapp link Button---------------- */}
 
-            <Grid item xs={2} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <Button
+            <Grid item xs={2} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormButton
                 variant="contained"
-                sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+                sx={{ width: '8.5rem', margin: '1em 0.5em !important' }}
                 color="success"
                 startIcon={<WhatsApp />}
                 onClick={openWhatsApp}
-                disabled={props.type === "view"}
+                disabled={props.type === 'view'}
               >
                 WhatsApp
-              </Button>
+              </FormButton>
             </Grid>
 
             {/* ---------------Category Field---------------- */}
 
-            <Grid item xs={6} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <TextField
+            <Grid item xs={6} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormTextField
                 select
                 variant="outlined"
-                {...formik.getFieldProps("selectedPath")}
+                {...formik.getFieldProps('selectedPath')}
                 onChange={handlePathChange}
                 label="Category"
                 required
-                sx={{ width: "100%" }}
+                sx={{ width: '100%' }}
               >
                 <MenuItem value="images">Image</MenuItem>
                 <MenuItem value="documents">Document</MenuItem>
                 <MenuItem value="others">Other</MenuItem>
-              </TextField>
+              </FormTextField>
             </Grid>
 
             {/* ---------------AddedBy Field---------------- */}
 
-            <Grid item xs={6} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <TextField
+            <Grid item xs={6} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormTextField
                 variant="outlined"
                 label="AddedBy"
-                sx={{ width: "100%" }}
+                sx={{ width: '100%' }}
                 placeholder="Add a consumer"
                 required
-                {...formik.getFieldProps("addedBy")}
+                {...formik.getFieldProps('addedBy')}
                 error={formik.touched.addedBy && Boolean(formik.errors.addedBy)}
                 helperText={formik.touched.addedBy && formik.errors.addedBy}
-                disabled={props.type === "view"}
+                disabled={props.type === 'view'}
               />
             </Grid>
 
             {/* ---------------AddedDate Field---------------- */}
 
-            <Grid item xs={5} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <TextField
+            <Grid item xs={5} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormTextField
                 variant="outlined"
                 type="date"
                 defaultValue=""
@@ -213,140 +217,140 @@ const AddTaskResources = (props) => {
                 InputLabelProps={{
                   shrink: true,
                 }}
-                sx={{ width: "100%" }}
-                {...formik.getFieldProps("addedDate")}
+                sx={{ width: '100%' }}
+                {...formik.getFieldProps('addedDate')}
                 error={
                   formik.touched.addedDate && Boolean(formik.errors.addedDate)
                 }
                 helperText={formik.touched.addedDate && formik.errors.addedDate}
-                disabled={props.type === "view"}
+                disabled={props.type === 'view'}
               />
             </Grid>
 
             {/* ---------------FileUpload Field---------------- */}
 
-            <Grid item xs={5} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <TextField
+            <Grid item xs={5} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormTextField
                 type="file"
-                sx={{ width: "100%" }}
-                {...formik.getFieldProps("selectedFile")}
+                sx={{ width: '100%' }}
+                {...formik.getFieldProps('selectedFile')}
                 onChange={handleFileChange}
                 required
-                disabled={props.type === "view"}
+                disabled={props.type === 'view'}
               />
             </Grid>
 
             {/* ---------------Upload link Button---------------- */}
 
-            <Grid item xs={2} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <Button
+            <Grid item xs={2} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormButton
                 variant="contained"
-                sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+                sx={{ width: '8.5rem', margin: '1em 0.5em !important' }}
                 color="success"
                 startIcon={<FileUpload />}
                 onClick={handleUpload}
-                disabled={props.type === "view"}
+                disabled={props.type === 'view'}
               >
                 Upload
-              </Button>
+              </FormButton>
             </Grid>
 
             {/* ---------------Comment Field---------------- */}
 
-            <Grid item xs={12} sx={{ padding: "1em 1em 0em 1em !important" }}>
-              <TextField
+            <Grid item xs={12} sx={{ padding: '1em 1em 0em 1em !important' }}>
+              <FormTextField
                 variant="outlined"
                 label="Comment"
-                sx={{ width: "100%" }}
+                sx={{ width: '100%' }}
                 multiline
                 required
                 rows={4}
                 placeholder="Add a comment"
-                {...formik.getFieldProps("comment")}
+                {...formik.getFieldProps('comment')}
                 error={formik.touched.comment && Boolean(formik.errors.comment)}
                 helperText={formik.touched.comment && formik.errors.comment}
-                disabled={props.type === "view"}
+                disabled={props.type === 'view'}
               />
             </Grid>
           </Grid>
 
-          {props.type === "add" && (
+          {props.type === 'add' && (
             <Box display="flex" width="70%" justifyContent="flex-end">
               <div
                 style={{
-                  display: "flex",
-                  justifyContent: "end",
-                  padding: "1em 2em 0em 2em !important",
+                  display: 'flex',
+                  justifyContent: 'end',
+                  padding: '1em 2em 0em 2em !important',
                 }}
               >
                 {/* ---------------Clear Button---------------- */}
 
-                <Button
+                <FormClearButton
                   variant="contained"
-                  sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+                  sx={{ width: '8.5rem', margin: '1em 0.5em !important' }}
                   color="primary"
                   startIcon={<ClearAllIcon />}
                   onClick={handleClear}
                 >
                   Clear
-                </Button>
+                </FormClearButton>
 
                 {/* ---------------Save Button---------------- */}
 
-                <Button
+                <FormSaveLoadingButton
                   color="primary"
                   onClick={formik.submitForm}
                   loading={formik.isSubmitting}
                   loadingPosition="start"
                   startIcon={<SaveIcon />}
                   variant="contained"
-                  sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+                  sx={{ width: '8.5rem', margin: '1em 0.5em !important' }}
                 >
                   Save
-                </Button>
+                </FormSaveLoadingButton>
               </div>
             </Box>
           )}
 
-          {props.type === "view" && (
+          {props.type === 'view' && (
             <Box display="flex" width="70%" justifyContent="flex-end">
               <div
                 style={{
-                  display: "flex",
-                  justifyContent: "end",
-                  padding: "1em 2em 0em 2em !important",
+                  display: 'flex',
+                  justifyContent: 'end',
+                  padding: '1em 2em 0em 2em !important',
                 }}
               >
                 {/* ---------------AddNewResource Button---------------- */}
 
-                <Button
+                <FormButton
                   variant="contained"
-                  sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+                  sx={{ width: '8.5rem', margin: '1em 0.5em !important' }}
                   color="primary"
                   onClick={() => navi(`/cia/resource/add/id`)}
                   startIcon={<AddBox />}
                 >
                   Add New
-                </Button>
+                </FormButton>
 
                 {/* ---------------ShowFiles Button---------------- */}
 
-                <Button
+                <FormButton
                   variant="contained"
-                  sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+                  sx={{ width: '8.5rem', margin: '1em 0.5em !important' }}
                   color="primary"
                   startIcon={<Visibility />}
                   onClick={openDrive}
                 >
                   Show Files
-                </Button>
+                </FormButton>
               </div>
             </Box>
           )}
         </Box>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default AddTaskResources;
+export default AddTaskResources
