be9b642a6e5dc933d2fdc957b413333e34476dbe
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-05 09:05:42 +0530

    Added styled components to task form

--- src/Pages/CIA/TaskStatus/TaskForm.jsx
+++ src/Pages/CIA/TaskStatus/TaskForm.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { statuses } from '../TaskData';
-import { useFormik } from 'formik';
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import MenuItem from '@mui/material/MenuItem'
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
+import { statuses } from '../TaskData'
+import { useFormik } from 'formik'
 import * as yup from 'yup'
-import { ClearAll, Save } from '@mui/icons-material';
+import { ClearAll, Save } from '@mui/icons-material'
+import FormTextField from '../../../Components/StyledComponents/FormTextField'
+import FormClearButton from '../../../Components/StyledComponents/FormClearButton'
+import FormSaveLoadingButton from '../../../Components/StyledComponents/FormSaveLoadingButton'
 
-{/* ---------------- Validation part ------------------ */}
+{
+  /* ---------------- Validation part ------------------ */
+}
 const taskStatusValidation = yup.object().shape({
-  status: yup.string().required('Required')
-  
+  status: yup.string().required('Required'),
 })
 
 function TaskForm() {
-
   const {
     values,
     errors,
@@ -26,52 +29,48 @@ function TaskForm() {
     handleSubmit,
     handleReset,
     submitForm,
-    } = useFormik({
-    initialValues :{
-      
-      status :'',
-      comment :''
-
+  } = useFormik({
+    initialValues: {
+      status: '',
+      comment: '',
     },
     onSubmit: (values) => {
       console.log('form values', values)
     },
 
-    validationSchema:taskStatusValidation
+    validationSchema: taskStatusValidation,
   })
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-
-      <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '45ch' },
-        }}
-        noValidate
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '45ch' },
+          }}
+          noValidate
           autoComplete="off"
           height="50vh"
           flexDirection="column"
-      >   
+        >
           {/* ---------------- Task field ------------------ */}
           <div>
-            <TextField
-                disabled
-                id="task"
-                name='task'
-                label="Task ID"
-                value={values.task}
-            >
-          </TextField>
+            <FormTextField
+              disabled
+              id="task"
+              name="task"
+              label="Task ID"
+              value={values.task}
+            ></FormTextField>
           </div>
 
           {/* ---------------- Status field ------------------ */}
           <div>
-          <TextField
+            <FormTextField
               required
               id="status"
-              name='status'
+              name="status"
               select
               label="Status"
               value={values.status}
@@ -79,42 +78,49 @@ function TaskForm() {
               error={touched.status && errors.status}
               helperText={touched.status ? errors.status : ''}
               onChange={handleChange}
-                
-          >
-            {statuses.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
+            >
+              {statuses.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </FormTextField>
           </div>
 
           {/* ---------------- comment field ------------------ */}
           <div>
-          <TextField
-            id="comment"
-            name='comment'
-            label="Comment"
-            multiline
-            rows={5}
-            value={values.comment}
-            onChange={handleChange}
-          />
+            <FormTextField
+              id="comment"
+              name="comment"
+              label="Comment"
+              multiline
+              rows={5}
+              value={values.comment}
+              onChange={handleChange}
+            />
           </div>
 
           {/* ---------------- Button placement ------------------ */}
           <div>
-              <Stack spacing={2} direction="row" justifyContent="flex-end">
-                  
-                  <Button  variant="contained" startIcon={<ClearAll/>} onClick={handleReset}>  Clear  </Button>
-                  <Button  variant="contained" startIcon={<Save/>} onClick={submitForm}>  Save  </Button>
-                  
-              </Stack>
+            <Stack spacing={2} direction="row" justifyContent="flex-end">
+              <FormClearButton
+                variant="contained"
+                startIcon={<ClearAll />}
+                onClick={handleReset}
+              >
+                Clear
+              </FormClearButton>
+              <FormSaveLoadingButton
+                variant="contained"
+                startIcon={<Save />}
+                onClick={submitForm}
+              >
+                Save
+              </FormSaveLoadingButton>
+            </Stack>
           </div>
-      </Box>
-
+        </Box>
       </form>
-    
     </>
   )
 }
