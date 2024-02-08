ab90e3d5666652c41a9cce77f3e18957d0ddcdf4
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-02-08 15:42:08 +0530

    changed systemuservalidations

--- src/Validation/SystemUserValidation.jsx
+++ src/Validation/SystemUserValidation.jsx
@@ -1,14 +1,14 @@
 import * as yup from 'yup';
 
 const SystemUserValidation = yup.object().shape({
-  FirstName: yup.string().required('First Name is required'),
-  LastName: yup.string().required('Last Name is required'),
-  Address: yup.string().required('Address is required'),
-  Email: yup.string().email('Invalid email').required('Email is required'),
-  MobileNo: yup.string().required('Mobile No is required'),
-  UserName: yup.string().required('Username is required'),
-  Password: yup.string().required('Password is required'),
-  Role: yup.string().required('Role is required'),
+  firstName: yup.string().required('First Name is required'),
+  lastName: yup.string().required('Last Name is required'),
+  address: yup.string().required('Address is required'),
+  email: yup.string().email('Invalid email').required('Email is required'),
+  mobileNo: yup.string().required('Mobile No is required'),
+  userName: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required'),
+  role: yup.string().required('Role is required'),
 });
 
 export default SystemUserValidation;
