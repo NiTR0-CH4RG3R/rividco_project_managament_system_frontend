5068465feaded696ac12699e9ac491484a7adab3
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-08 14:20:14 +0530

    Added some styles to task table

--- src/Pages/CIA/TaskStatus/TaskTable.jsx
+++ src/Pages/CIA/TaskStatus/TaskTable.jsx
@@ -1,6 +1,10 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+} from '@mui/x-data-grid'
 
 const columns = [
   {
@@ -26,23 +30,81 @@ const columns = [
     headerName: 'Comment',
     width: 300,
     editable: true,
-  }
-];
-
+  },
+]
 
 const rows = [
-  { id:'11' ,taskStatus: 'Active', addedDate: '01/10/2024', addedBy: 'Admin 01' ,comment:'Comment 01' },
-  { id:'12' ,taskStatus: 'OnHold', addedDate: '01/10/2024', addedBy: 'Admin 02' ,comment:'Comment 02' },
-  { id:'13' ,taskStatus: 'Reject', addedDate: '01/10/2024', addedBy: 'Admin 01' ,comment:'Comment 03' },
-  { id:'14' ,taskStatus: 'Active', addedDate: '01/10/2024', addedBy: 'Admin 01' ,comment:'Comment 04' },
-  { id:'11' ,taskStatus: 'Wating', addedDate: '01/10/2024', addedBy: 'Admin 07' ,comment:'Comment 05' },
-  { id:'11' ,taskStatus: 'Active', addedDate: '01/10/2024', addedBy: 'Admin 01' ,comment:'Comment 06' },
-  { id:'12' ,taskStatus: 'OnHold', addedDate: '01/10/2024', addedBy: 'Admin 01' ,comment:'Comment 07' },
-  { id:'13' ,taskStatus: 'Reject', addedDate: '01/10/2024', addedBy: 'Admin 09' ,comment:'Comment 08' },
-  { id:'14' ,taskStatus: 'Active', addedDate: '01/10/2024', addedBy: 'Admin 01' ,comment:'Comment 09' },
-  { id:'11' ,taskStatus: 'Wating', addedDate: '01/10/2024', addedBy: 'Admin 08' ,comment:'Comment 10' },
-  
-];
+  {
+    id: '11',
+    taskStatus: 'Active',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 01',
+    comment: 'Comment 01',
+  },
+  {
+    id: '12',
+    taskStatus: 'OnHold',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 02',
+    comment: 'Comment 02',
+  },
+  {
+    id: '13',
+    taskStatus: 'Reject',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 01',
+    comment: 'Comment 03',
+  },
+  {
+    id: '14',
+    taskStatus: 'Active',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 01',
+    comment: 'Comment 04',
+  },
+  {
+    id: '11',
+    taskStatus: 'Wating',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 07',
+    comment: 'Comment 05',
+  },
+  {
+    id: '11',
+    taskStatus: 'Active',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 01',
+    comment: 'Comment 06',
+  },
+  {
+    id: '12',
+    taskStatus: 'OnHold',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 01',
+    comment: 'Comment 07',
+  },
+  {
+    id: '13',
+    taskStatus: 'Reject',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 09',
+    comment: 'Comment 08',
+  },
+  {
+    id: '14',
+    taskStatus: 'Active',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 01',
+    comment: 'Comment 09',
+  },
+  {
+    id: '11',
+    taskStatus: 'Wating',
+    addedDate: '01/10/2024',
+    addedBy: 'Admin 08',
+    comment: 'Comment 10',
+  },
+]
 
 // const columns = [
 //   {
@@ -77,11 +139,10 @@ function CustomToolbar() {
     <GridToolbarContainer>
       <GridToolbarExport />
     </GridToolbarContainer>
-  );
+  )
 }
 
 export default function TaskTable() {
-
   // const [rows, setRows] = useState([])
 
   // useEffect(() => {
@@ -97,30 +158,30 @@ export default function TaskTable() {
   // }, [])
 
   return (
-    <Box 
-    noValidate
+    <Box
+      noValidate
       autoComplete="off"
       display="flex"
       justifyContent="center"
       alignItems="center"
       height="80vh"
-      flexDirection="column" 
-      sx={{ height: 630, width: '100%' }}>
-
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 10,
-              },
+      flexDirection="column"
+      sx={{ height: 630, width: '100%' }}
+    >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
             },
-          }}
-          pageSizeOptions={[5, 10, 25]}
-          disableRowSelectionOnClick
-          slots={{ toolbar: CustomToolbar }}
-        />
+          },
+        }}
+        pageSizeOptions={[5, 10, 25]}
+        disableRowSelectionOnClick
+        slots={{ toolbar: CustomToolbar }}
+      />
     </Box>
-  );
+  )
 }
