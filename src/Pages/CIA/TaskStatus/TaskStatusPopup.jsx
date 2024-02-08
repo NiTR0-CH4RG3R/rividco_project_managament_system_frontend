2fc4f7ea4ad8afab9c13fdda46ea0d7ab4c24904
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-08 14:19:49 +0530

    Added a new dialog component and included the form component inside it.

---
+++ src/Pages/CIA/TaskStatus/TaskStatusPopup.jsx
@@ -0,0 +1,15 @@
+import { Dialog, DialogContent, DialogTitle } from '@mui/material'
+import React from 'react'
+import TaskForm from './TaskForm'
+
+export default function TaskStatusPopup(props) {
+  const { openPopUp, setOpenPopup } = props
+  return (
+    <Dialog open={openPopUp} onClose={() => setOpenPopup(false)}>
+      <DialogTitle>Add New Task Status</DialogTitle>
+      <DialogContent>
+        <TaskForm />
+      </DialogContent>
+    </Dialog>
+  )
+}
