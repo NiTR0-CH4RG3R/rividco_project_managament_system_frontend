f696dfc60edf3d3c91dca443296e7f56e810b38d
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-11 18:59:52 +0530

    The new axios

---
+++ src/api/axios.js
@@ -0,0 +1,30 @@
+import Axios from 'axios';
+
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
+const axios = Axios.create({
+    baseURL: baseURL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+
+// This axios is used to send login and refresh requests to the server.
+// This instance will not be using the interceptors.
+// Also this instance will be using withCredentials: true to send the http onlys cookies to the server.
+const secretAxios = Axios.create({
+    baseURL: baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true
+});
+
+export const get = async (url, params) => { return await axios.get(url, { params }) };
+export const post = async (url, data, params) => { return await axios.post(url, data, { params }) };
+export const put = async (url, data, params) => { return await axios.put(url, data, { params }) };
+export const del = async (url, params) => { return await axios.delete(url, { params }) };
+
+export const secretGet = async (url, params) => { return await secretAxios.get(url, { params }) };
+export const secretPost = async (url, data, params) => { return await secretAxios.post(url, data, { params }) };
