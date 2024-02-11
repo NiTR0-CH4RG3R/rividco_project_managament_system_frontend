1f0f03487c686ee1e1cb42867a946de0e984ae72
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-11 18:59:36 +0530

    Deleted old api layer

--- src/api/customerAPI.js
+++
@@ -1,9 +0,0 @@
-import { getRequest, postRequest, putRequest, delRequest } from './common/axios';
-
-const CUSTOMER_URL = '/customers';
-
-export const getCustomers = (params) => { return getRequest(CUSTOMER_URL, params); }
-export const getCustomer = (id, params) => { return getRequest(`${CUSTOMER_URL}/${id}`, params); }
-export const createCustomer = (data, params) => { return postRequest(CUSTOMER_URL, data, params); }
-export const updateCustomer = (id, data, params) => { return putRequest(`${CUSTOMER_URL}/${id}`, data, params); }
-export const deleteCustomer = (id, params) => { return delRequest(`${CUSTOMER_URL}/${id}`, params); }
--- src/api/systemUserAPI.js
+++
@@ -1,9 +0,0 @@
-import { getRequest, postRequest, putRequest, delRequest } from './common/axios';
-
-const SYSTEM_USER_URL = '/systemUsers';
-
-export const getSystemUsers = (params) => { return getRequest(SYSTEM_USER_URL, params); }
-export const getSystemUser = (id, params) => { return getRequest(`${SYSTEM_USER_URL}/${id}`, params); }
-export const createSystemUser = (data, params) => { return postRequest(SYSTEM_USER_URL, data, params); }
-export const updateSystemUser = (id, data, params) => { return putRequest(`${SYSTEM_USER_URL}/${id}`, data, params); }
-export const deleteSystemUser = (id, params) => { return delRequest(`${SYSTEM_USER_URL}/${id}`, params); }
