8f9e982aa79516dc64d4b3e9927e0129a9b3feec
sCodeu <sachindu99u@gmail.com>
2024-02-12 09:46:51 +0530

    Change every id to :id

--- src/Data/AppRoutes.js
+++ src/Data/AppRoutes.js
@@ -32,19 +32,19 @@ export const AppRoutes = {
     /* Vendor related routes */
     vendor_list: { path: "/vendor/list", component: <VendorList /> },
     vendor_add: { path: "/vendor/add", component: <Vendor type="add" /> },
-    vendor_view: { path: "/vendor/view/id", component: <Vendor type="view" /> },
+    vendor_view: { path: "/vendor/view/:id", component: <Vendor type="view" /> },
     vendor_edit: { path: "/vendor/edit", component: <Vendor type="edit" /> },
 
     /* Vendor Items related routes */
     vendor_item_list: { path: "/vendor/item/list", component: <VendorItemList />, },
     vendor_item_add: { path: "/vendor/item/add", component: <Vendoritem type="add" />, },
-    vendor_item_view: { path: "/vendor/item/view/id", component: <Vendoritem type="view" />, },
+    vendor_item_view: { path: "/vendor/item/view/:id", component: <Vendoritem type="view" />, },
     vendor_item_edit: { path: "/vendor/item/edit", component: <Vendoritem type="edit" />, },
 
     /* Project related routes */
     project_list: { path: "/project/list", component: <ProjectList />, },
     project_add: { path: "/project/add", component: <Project type="add" /> },
-    project_view: { path: "/project/view/id", component: <Project type="view" />, },
+    project_view: { path: "/project/view/:id", component: <Project type="view" />, },
     project_edit: { path: "/project/edit", component: <Project type="edit" />, },
     project_services_list: { path: "/project/services/list", component: <> </>, },
     project_test_list: { path: "/project/test", component: <ProjectTest />, },
@@ -53,15 +53,15 @@ export const AppRoutes = {
     /* CIA related routes */
     cia_list: { path: "/cia/list", component: <TaskList /> },
     cia_add: { path: "/cia/add", component: <Task type="add" /> },
-    cia_view: { path: "/cia/view/id", component: <Task type="view" /> },
-    cia_edit: { path: "/cia/edit/id", component: <Task type="edit" /> },
-    cia_resources_add: { path: "/cia/resource/add/id", component: <AddTaskResources type="add" />, },
-    cia_resources_view: { path: "/cia/resource/view/id", component: <AddTaskResources type="view" />, },
+    cia_view: { path: "/cia/view/:id", component: <Task type="view" /> },
+    cia_edit: { path: "/cia/edit/:id", component: <Task type="edit" /> },
+    cia_resources_add: { path: "/cia/resource/add/:id", component: <AddTaskResources type="add" />, },
+    cia_resources_view: { path: "/cia/resource/view/:id", component: <AddTaskResources type="view" />, },
     cia_status: { path: "/cia/status", component: <TaskStatus /> },
 
     /* System User related routes */
     system_user_list: { path: "/user/list", component: <SystemUserList /> },
     system_user_add: { path: "/user/add", component: <SystemUser type="add" /> },
-    system_user_view: { path: "/user/view/id", component: <SystemUser type="view" />, },
-    system_user_edit: { path: "/user/edit/id", component: <SystemUser type="edit" />, },
+    system_user_view: { path: "/user/view/:id", component: <SystemUser type="view" />, },
+    system_user_edit: { path: "/user/edit/:id", component: <SystemUser type="edit" />, },
 };
