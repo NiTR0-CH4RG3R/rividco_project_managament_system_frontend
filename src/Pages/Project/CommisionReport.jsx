a45a34fdf46d47634347a23de7863578d92f8363
sCodeu <sachindu99u@gmail.com>
2024-02-13 12:00:09 +0530

    Name Table Coloums

--- src/Pages/Project/CommisionReport.jsx
+++ src/Pages/Project/CommisionReport.jsx
@@ -1,4 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTopbarContext } from '../../Contexts/TopbarContext';
+import ListPage from '../../Components/ListPage/ListPage';
+import { AppRoutes } from '../../Data/AppRoutes';
+
+const columns = [
+    { id: 'fileName', label: 'File Name', align: 'left' },
+    { id: 'addedDate', label: 'Added Date', align: 'left' },
+    
+];
 
 export default function CommisionReport() {
   return (
