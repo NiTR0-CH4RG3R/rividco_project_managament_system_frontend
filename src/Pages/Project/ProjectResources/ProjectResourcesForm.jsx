9de995f19300f7ab8b1d1d2320256fac38644d2c
Yashod Rathnayake <144260555+YashodR@users.noreply.github.com>
2024-02-13 11:26:17 +0530

    added file upload button in project resources

--- src/Pages/Project/ProjectResources/ProjectResourcesForm.jsx
+++ src/Pages/Project/ProjectResources/ProjectResourcesForm.jsx
@@ -8,12 +8,26 @@ import { category } from './ProjectResourcesData';
 import { useFormik } from 'formik';
 import * as yup from 'yup'
 import { ClearAll, Save } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
  /* ---------------- Validation part ------------------ */
 const ProjectResourcesValidation = yup.object().shape({
   status: yup.string().required('Required')
   
 })
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 function ProjectResourcesForm() {
 
@@ -106,6 +120,18 @@ function ProjectResourcesForm() {
           </div>
 
           {/* ---------------- Button placement ------------------ */}
+
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload file
+            <VisuallyHiddenInput type="file" />
+           </Button>
+
           <div>
               <Stack spacing={1} direction="row" justifyContent="flex-end">
                   
