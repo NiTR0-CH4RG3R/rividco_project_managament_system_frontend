872604c6859c3f9ffff9b9189a113cc75b0c7576
sCodeu <sachindu99u@gmail.com>
2024-02-14 21:42:29 +0530

    Added UseEfect

--- src/Pages/Vendor/VendorList.jsx
+++ src/Pages/Vendor/VendorList.jsx
@@ -1,55 +1,88 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTopbarContext } from '../../Contexts/TopbarContext';
-import ListPage from '../../Components/ListPage/ListPage';
-import { AppRoutes } from '../../Data/AppRoutes';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTopbarContext } from "../../Contexts/TopbarContext";
+import ListPage from "../../Components/ListPage/ListPage";
+import { AppRoutes } from "../../Data/AppRoutes";
+import * as vendorService from "../../services/vendorService";
 
 const columns = [
-    { id: 'registration', label: 'Vendor Registration No.', align: 'left' },
-    { id: 'name', label: 'Name', align: 'left' },
-    { id: 'address', label: 'Address', align: 'left' },
-    { id: 'email', label: 'Email', align: 'left' },
-    { id: 'contact', label: 'Contact Number', align: 'right' },
+  { id: "registration", label: "Vendor Registration No.", align: "left" },
+  { id: "name", label: "Name", align: "left" },
+  { id: "address", label: "Address", align: "left" },
+  { id: "email", label: "Email", align: "left" },
+  { id: "contact", label: "Contact Number", align: "right" },
 ];
 
 export default function VendorList() {
-    const { setTitle, setSubtitle } = useTopbarContext();
-    setTitle("List Vendors");
-    setSubtitle("You can view and manage all the vendors here.");
+  const { setTitle, setSubtitle } = useTopbarContext();
+  setTitle("List Vendors");
+  setSubtitle("You can view and manage all the vendors here.");
 
-    const [rows, setRows] = useState([
-        { id: 0, registration: 'V001', name: 'NC Enterprices', address: 'No. 380, Walawwaththa, Dadalla, Galle', email: 'idontknow@gmail.com', contact: '1234567890', }
-    ]);
+  const [rows, setRows] = useState([
+    {
+      id: 0,
+      registration: "V001",
+      name: "NC Enterprices",
+      address: "No. 380, Walawwaththa, Dadalla, Galle",
+      email: "idontknow@gmail.com",
+      contact: "1234567890",
+    },
+  ]);
 
-    const[page,setPage]=useState(0);
-    const[rowsPerPage,setRowsPerPage]=useState(5);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    setPage(0);
+    setRowsPerPage(5);
+  }, []);
 
-    return (
-        <ListPage
-            columns={columns}
-            rows={rows}
-            searchBarProps={{
-                searchBy: 'name',
-                onSearchChange: (e) => { console.log(e.target.value); },
-                onSearchClick: (e) => { },
-            }}
-            onRowClick={(e, id) => {
-                console.log(id);
-            }}
-            onAddButtonClick={(e) => {
-                navigate(AppRoutes.vendor_add.path)
-            }}
-            tablePaginationProps={{
-                rowsPerPageOptions: [5, 10, 25, 100],
-                component: "div",
-                rowsPerPage: 5,
-                page: 0,
-                count: 100,
-                onPageChange: (e, page) => { console.log(page); },
-                onRowsPerPageChange: (e) => { console.log(e.target.value); }
-            }}
-        />
-    );
+  useEffect(() => {
+    vendorService.listVendors(page + 1, rowsPerPage)
+    .then(
+        vendors =>{
+            setRows(vendors);
+        }
+    )
+    .catch(
+        error =>{
+            console.log(error);
+        }
+    )
+  }, [page, rowsPerPage]);
+
+  const navigate = useNavigate();
+
+  return (
+    <ListPage
+      columns={columns}
+      rows={rows}
+      searchBarProps={{
+        searchBy: "name",
+        onSearchChange: (e) => {
+          console.log(e.target.value);
+        },
+        onSearchClick: (e) => {},
+      }}
+      onRowClick={(e, id) => {
+        console.log(id);
+      }}
+      onAddButtonClick={(e) => {
+        navigate(AppRoutes.vendor_add.path);
+      }}
+      tablePaginationProps={{
+        rowsPerPageOptions: [5, 10, 25, 100],
+        component: "div",
+        rowsPerPage: 5,
+        page: 0,
+        count: 100,
+        onPageChange: (e, page) => {
+          console.log(page);
+        },
+        onRowsPerPageChange: (e) => {
+          console.log(e.target.value);
+        },
+      }}
+    />
+  );
 }
