288db3984d05fd43e7a2025a3a360d8556253803
sCodeu <sachindu99u@gmail.com>
2024-02-14 23:00:07 +0530

    Listvendoritem func

---
+++ src/services/vendorItemService.js
@@ -0,0 +1,28 @@
+import { get, post, put, del } from '../api/axios'
+
+const VENDORITEM_URL='/vendor/item'
+
+export async function listVendorItems(page, itemsPerPage) {
+    const vendorItems = [];
+
+    try {
+        const response = await get(VENDORITEM_URL, { page, pageSize: itemsPerPage });
+        response?.data?.forEach((vendorItem) => {
+            vendorItems.push({
+                id: vendorItem.id,
+                vendorId:vendorItem.vendorId,
+                productName: vendorItem.productName,
+                price: vendorItem.price,
+                warrantyDuration:vendorItem.warrantyDuration,
+                capacity:vendorItem.capacity,
+
+
+            })
+        })
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return vendorItems;
+}
