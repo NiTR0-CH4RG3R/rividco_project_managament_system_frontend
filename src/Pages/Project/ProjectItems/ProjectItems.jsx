4be9b648ada1b4b064b115b79a214811f5f74240
sCodeu <sachindu99u@gmail.com>
2024-02-14 13:52:15 +0530

    Added popup window to the add btn

--- src/Pages/Project/ProjectItems/ProjectItems.jsx
+++ src/Pages/Project/ProjectItems/ProjectItems.jsx
@@ -1,58 +1,71 @@
-import React, { useState } from 'react'
-import { useTopbarContext } from '../../../Contexts/TopbarContext'
-import ProjectItemsPopup from '../ProjectItems/ProjectItemsPopup'
-import ListPage from '../../../Components/ListPage/ListPage';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useTopbarContext } from "../../../Contexts/TopbarContext";
+import ProjectItemsPopup from "../ProjectItems/ProjectItemsPopup";
+import ListPage from "../../../Components/ListPage/ListPage";
+import { useNavigate } from "react-router-dom";
 
 const columns = [
-    { id: 'vendorItem', label: 'Vendor Item', align: 'left' },
-    { id: 'serialNumber', label: 'Serial Number', align: 'left' },
-    { id: 'warrantyPeriod', label: 'Warranty Period', align: 'left' },
-    { id: 'conductedDate', label: 'Conducted Date', align: 'left' },
-    { id: 'comment', label: 'Comment', align: 'center' },
+  { id: "vendorItem", label: "Vendor Item", align: "left" },
+  { id: "serialNumber", label: "Serial Number", align: "left" },
+  { id: "warrantyPeriod", label: "Warranty Period", align: "left" },
+  { id: "conductedDate", label: "Conducted Date", align: "left" },
+  { id: "comment", label: "Comment", align: "center" },
 ];
 
 export default function ProjectItems() {
-    const { setTitle, setSubtitle } = useTopbarContext();
-    setTitle("Project items");
-    setSubtitle("You can view and manage all the project items here.");
+  const { setTitle, setSubtitle } = useTopbarContext();
+  setTitle("Project items");
+  setSubtitle("You can view and manage all the project items here.");
 
-    const [openPopUp, setOpenPopup] = useState(false)
+  const [openPopUp, setOpenPopup] = useState(false);
 
-    const [rows, setRows] = useState([
-        { vendorItem: 'item 1', serialNumber: '111QWERT', warrantyPeriod: '2 Months', conductedDate: '09/09/2023', comment: 'comment 1', },
-    ]);
+  const [rows, setRows] = useState([
+    {
+      vendorItem: "item 1",
+      serialNumber: "111QWERT",
+      warrantyPeriod: "2 Months",
+      conductedDate: "09/09/2023",
+      comment: "comment 1",
+    },
+  ]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-return (
-    <ListPage
+  return (
+    <>
+      <ListPage
         columns={columns}
         rows={rows}
         searchBarProps={{
-            searchBy: 'location',
-            onSearchChange: (e) => { console.log(e.target.value); },
-            onSearchClick: (e) => { },
+          searchBy: "location",
+          onSearchChange: (e) => {
+            console.log(e.target.value);
+          },
+          onSearchClick: (e) => {},
         }}
         onRowClick={(e, id) => {
-            console.log(id);
+          console.log(id);
         }}
         onAddButtonClick={(e) => {
-           <ProjectItemsPopup/>
+          setOpenPopup(true);
         }}
         tablePaginationProps={{
-            rowsPerPageOptions: [5, 10, 25, 100],
-            component: "div",
-            rowsPerPage: 5,
-            page: 0,
-            count: 100,
-            onPageChange: (e, page) => { console.log(page); },
-            onRowsPerPageChange: (e) => { console.log(e.target.value); }
+          rowsPerPageOptions: [5, 10, 25, 100],
+          component: "div",
+          rowsPerPage: 5,
+          page: 0,
+          count: 100,
+          onPageChange: (e, page) => {
+            console.log(page);
+          },
+          onRowsPerPageChange: (e) => {
+            console.log(e.target.value);
+          },
         }}
         disableSearchBar
         // customUpperBar={<UpperBar />}
-    />
-);
+      />
+      <ProjectItemsPopup openPopUp={openPopUp} setOpenPopup={setOpenPopup} />
+    </>
+  );
 }
-
-
