f669243da2cc71f20528964fedf8a540ffef6e4e
Yashod Rathnayake <144260555+YashodR@users.noreply.github.com>
2024-02-14 13:38:26 +0530

    project items page updated

--- src/Pages/Project/ProjectItems/ProjectItems.jsx
+++ src/Pages/Project/ProjectItems/ProjectItems.jsx
@@ -1,47 +1,56 @@
-import { Box, Grid } from '@mui/material'
 import React, { useState } from 'react'
-import ProjectTable from '../ProjectItems/ProjectItemsTable'
 import { useTopbarContext } from '../../../Contexts/TopbarContext'
-import FormSaveLoadingButton from '../../../Components/StyledComponents/FormSaveLoadingButton'
 import ProjectItemsPopup from '../ProjectItems/ProjectItemsPopup'
+import ListPage from '../../../Components/ListPage/ListPage';
+import { useNavigate } from 'react-router-dom';
 
+const columns = [
+    { id: 'vendorItem', label: 'Vendor Item', align: 'left' },
+    { id: 'serialNumber', label: 'Serial Number', align: 'left' },
+    { id: 'warrantyPeriod', label: 'Warranty Period', align: 'left' },
+    { id: 'conductedDate', label: 'Conducted Date', align: 'left' },
+    { id: 'comment', label: 'Comment', align: 'center' },
+];
 
 export default function ProjectItems() {
     const { setTitle, setSubtitle } = useTopbarContext();
     setTitle("Project items");
     setSubtitle("You can view and manage all the project items here.");
 
-    const [openPopUp, setOpenPopup] = useState(false)
+    const [rows, setRows] = useState([
+        { vendorItem: 'item 1', serialNumber: '111QWERT', warrantyPeriod: '2 Months', conductedDate: '09/09/2023', comment: 'comment 1', },
+    ]);
 
+    const navigate = useNavigate();
 
-    return (
-        <div>
-
-            <Box>
-                <Grid container spacing={1} sx={{ width: "100%" }}>
-
-                    {/* ---------------- Task Table ------------------ */}
-                    <Grid item xs={8} sx={{ padding: "4em 2em 0em 4em !important" }}>
-                        <ProjectTable />
-                    </Grid>
-
-
-                    {/* ---------------- Task button ------------------ */}
-                    <Grid item xs={4} sx={{ padding: "4em 2em 0em 6em !important" }}>
-
-                    <FormSaveLoadingButton onClick={() => setOpenPopup(true)}>
-              Add New Item
-            </FormSaveLoadingButton>
-                    </Grid>
-
-                 </Grid>
-        
-        <ProjectItemsPopup openPopUp={openPopUp} setOpenPopup={setOpenPopup} />
-                
-            </Box>
-
-        </div>
-    );
+return (
+    <ListPage
+        columns={columns}
+        rows={rows}
+        searchBarProps={{
+            searchBy: 'location',
+            onSearchChange: (e) => { console.log(e.target.value); },
+            onSearchClick: (e) => { },
+        }}
+        onRowClick={(e, id) => {
+            console.log(id);
+        }}
+        onAddButtonClick={(e) => {
+           <ProjectItemsPopup/>
+        }}
+        tablePaginationProps={{
+            rowsPerPageOptions: [5, 10, 25, 100],
+            component: "div",
+            rowsPerPage: 5,
+            page: 0,
+            count: 100,
+            onPageChange: (e, page) => { console.log(page); },
+            onRowsPerPageChange: (e) => { console.log(e.target.value); }
+        }}
+        disableSearchBar
+        // customUpperBar={<UpperBar />}
+    />
+);
 }
 
 
