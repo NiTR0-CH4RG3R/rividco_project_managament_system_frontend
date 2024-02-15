8324e86e693bf4796f97779ff0f6582184e21c4c
sCodeu <sachindu99u@gmail.com>
2024-02-16 00:23:10 +0530

    Added DATA to the employee modal

--- src/Components/ModalWindow/EmployeeModal.jsx
+++ src/Components/ModalWindow/EmployeeModal.jsx
@@ -4,111 +4,100 @@ import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import CloseIcon from "@mui/icons-material/Close";
+import * as systemUserService from "../../services/systemUserService";
 
 export default function EmployeeModal(props) {
-  const { openEmployee, setOpenEmployee, sendData } = props
+  const { openEmployee, setOpenEmployee, sendData } = props;
 
   const employeecols = [
-    {
-      field: 'userId',
-      headerName: 'User ID',
-      align: 'center',
-      headerAlign: 'center',
-    },
-    {
-      field: 'id',
-      headerName: 'ID',
-      align: 'center',
-      headerAlign: 'center',
-    },
-    {
-      field: 'title',
-      headerName: 'Title',
-      align: 'center',
-      headerAlign: 'center',
-    },
-    {
-      field: 'completed',
-      headerName: 'Completed',
-      align: 'center',
-      headerAlign: 'center',
-    },
-  ]
+    { field: "firstName", headerName: "First Name", align: "left" },
+    { field: "lastName", headerName: "Last Name", align: "left" },
+    { field: "category", headerName: "Category", align: "left" },
+    { field: "address", headerName: "Address", align: "left" },
+    { field: "contact", headerName: "Contact Number" },
+  ];
 
   const handleClose = () => {
     setOpenEmployee(false);
   };
 
   const [rows, setRows] = useState([]);
+  const [loadidng, setLoading] = useState(false);
 
   useEffect(() => {
-    // Fetch data from REST endpoint using axios
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => {
-        setRows(response.data);
+    setLoading(true);
+    systemUserService
+      .listAllSystemUsers()
+      .then((employee) => {
+        setRows(employee);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error(error);
+        setLoading(false);
       });
   }, []);
 
   return (
-    <Modal open={openEmployee} onClose={handleClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          minWidth: 800,
-          minHeight: 400,
-          bgcolor: "background.paper",
-          boxShadow: 24,
-          p: 4,
-        }}
-      >
-        <Typography variant="h6">Employee Modal</Typography>
+    <>
+      loadidng ?<div>Loading...</div>:
+      <Modal open={openEmployee} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            minWidth: 800,
+            minHeight: 400,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h6">Employee Modal</Typography>
 
-        <div style={{ height: 400, width: '100%' }}>
-          <DataGrid
-            rows={rows}
-            columns={employeecols}
-            disableColumnFilter
-            disableColumnSelector
-            disableDensitySelector
-            slots={{ toolbar: GridToolbar }}
-            slotProps={{
-              toolbar: {
-                showQuickFilter: true,
-              },
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={employeecols}
+              disableColumnFilter
+              disableColumnSelector
+              disableDensitySelector
+              slots={{ toolbar: GridToolbar }}
+              slotProps={{
+                toolbar: {
+                  showQuickFilter: true,
+                },
+              }}
+              onRowClick={({ row }) => {
+                sendData("selectedEmployee", row);
+                setOpenEmployee(false);
+              }}
+
+              pagination
+            />
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              padding: "1em 2em 0em 2em !important",
             }}
-            onRowClick={({ row }) => {
-              sendData('selectedEmployee', row)
-              setOpenEmployee(false)
-            }}
-          />
-        </div>
-
-        <div
-                style={{
-                  display: "flex",
-                  justifyContent: "end",
-                  padding: "1em 2em 0em 2em !important",
-                }}
-              >
-                <Button
-                  variant="contained"
-                  sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
-                  color="primary"
-                  startIcon={<CloseIcon/>}
-                  onClick={handleClose}
-                >
-                  Close
-                </Button>
-              </div>
-
-      </Box>
-    </Modal>
+          >
+            <Button
+              variant="contained"
+              sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+              color="primary"
+              startIcon={<CloseIcon />}
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </>
   );
 }
