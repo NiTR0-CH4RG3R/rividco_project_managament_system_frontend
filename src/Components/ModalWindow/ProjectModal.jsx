8eee92a8aad371949953e4aa2c42cd92ab202a53
sCodeu <sachindu99u@gmail.com>
2024-02-16 00:37:06 +0530

    Added Data to the project modal

--- src/Components/ModalWindow/ProjectModal.jsx
+++ src/Components/ModalWindow/ProjectModal.jsx
@@ -2,37 +2,18 @@ import { Modal, Button } from "@mui/material";
 import { Box, Typography } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import * as projectService from "../../services/projectService";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function ProjectModal(props) {
   const { openProject, setOpenProject, sendData } = props;
 
   const projectcols = [
-    {
-      field: "userId",
-      headerName: "User ID",
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "id",
-      headerName: "ID",
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "title",
-      headerName: "Title",
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "completed",
-      headerName: "Completed",
-      align: "center",
-      headerAlign: "center",
-    },
+    { field: "projectId", headerName: "Project Id", align: "left" },
+    { field: "customer", headerName: "Customer", align: "left" },
+    { field: "location", headerName: "Location", align: "left" },
+    { field: "coordinator", headerName: "Coordinator", align: "left" },
+    { field: "status", headerName: "Status" },
   ];
 
   const handleClose = () => {
@@ -40,72 +21,79 @@ export default function ProjectModal(props) {
   };
 
   const [rows, setRows] = useState([]);
+  const [loadidng, setLoading] = useState(false);
 
   useEffect(() => {
-    // Fetch data from REST endpoint using axios
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => {
-        setRows(response.data);
+    setLoading(true);
+    projectService
+      .listAllProjects()
+      .then((project) => {
+        setRows(project);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error(error);
+        setLoading(false);
       });
   }, []);
 
   return (
-    <Modal open={openProject} onClose={handleClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          minWidth: 800,
-          minHeight: 400,
-          bgcolor: "background.paper",
-          boxShadow: 24,
-          p: 4,
-        }}
-      >
-        <Typography variant="h6">Project Modal</Typography>
-        <div style={{ height: 400, width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={projectcols}
-            disableColumnFilter
-            disableColumnSelector
-            disableDensitySelector
-            slots={{ toolbar: GridToolbar }}
-            slotProps={{
-              toolbar: {
-                showQuickFilter: true,
-              },
-            }}
-            onRowClick={({ row }) => {
-              sendData("selectedProject", row);
-              setOpenProject(false);
-            }}
-          />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "end",
-            padding: "1em 2em 0em 2em !important",
+    <>
+      loadidng ?<div>Loading...</div>:
+      <Modal open={openProject} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            minWidth: 800,
+            minHeight: 400,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
           }}
         >
-          <Button
-            variant="contained"
-            sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
-            color="primary"
-            startIcon={<CloseIcon />}
-            onClick={handleClose}
+          <Typography variant="h6">Project Modal</Typography>
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={projectcols}
+              disableColumnFilter
+              disableColumnSelector
+              disableDensitySelector
+              slots={{ toolbar: GridToolbar }}
+              slotProps={{
+                toolbar: {
+                  showQuickFilter: true,
+                },
+              }}
+              onRowClick={({ row }) => {
+                sendData("selectedProject", row);
+                setOpenProject(false);
+              }}
+              pagination
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              padding: "1em 2em 0em 2em !important",
+            }}
           >
-            Close
-          </Button>
-        </div>
-      </Box>
-    </Modal>
+            <Button
+              variant="contained"
+              sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+              color="primary"
+              startIcon={<CloseIcon />}
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </>
   );
 }
