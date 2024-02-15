5227a57fbf5c662206069abcddd066fa02e2ba9f
sCodeu <sachindu99u@gmail.com>
2024-02-16 01:04:37 +0530

    Added data but vendoralllist not found

--- src/Components/ModalWindow/VendorItemModal.jsx
+++ src/Components/ModalWindow/VendorItemModal.jsx
@@ -2,37 +2,18 @@ import { Modal, Button } from "@mui/material";
 import { Box, Typography } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import * as vendorItemService from "../../services/vendorItemService";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function VendorItemModal(props) {
   const { openVendorItem, setOpenVendorItem, sendData } = props;
 
   const vendoritemcols = [
-    {
-      field: "userId",
-      headerName: "Item ID",
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "id",
-      headerName: "ID",
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "title",
-      headerName: "Item Name",
-      align: "center",
-      headerAlign: "center",
-    },
-    {
-      field: "completed",
-      headerName: "Availability",
-      align: "center",
-      headerAlign: "center",
-    },
+    { field: "itemName", headerName: "Item Name", align: "left" },
+    { field: "vendor", headerName: "Vendor", align: "left" },
+    { field: "brand", headerName: "Brand", align: "left" },
+    { field: "price", headerName: "Price", align: "left" },
+    { field: "capacity", headerName: "Capacity" },
   ];
 
   const handleClose = () => {
@@ -40,74 +21,81 @@ export default function VendorItemModal(props) {
   };
 
   const [rows, setRows] = useState([]);
+  const [loadidng, setLoading] = useState(false);
 
   useEffect(() => {
-    // Fetch data from REST endpoint using axios
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => {
-        setRows(response.data);
+    setLoading(true);
+    /* vendorItemService
+      .listAllVendorItem()
+      .then((vendorItem) => {
+        setRows(vendorItem);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+        console.error(error);
+        setLoading(false);
+      });*/
   }, []);
 
   return (
-    <Modal open={openVendorItem} onClose={handleClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          minWidth: 800,
-          minHeight: 400,
-          bgcolor: "background.paper",
-          boxShadow: 24,
-          p: 4,
-        }}
-      >
-        <Typography variant="h6">Vendor Item Modal</Typography>
-
-        <div style={{ height: 400, width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={vendoritemcols}
-            disableColumnFilter
-            disableColumnSelector
-            disableDensitySelector
-            slots={{ toolbar: GridToolbar }}
-            slotProps={{
-              toolbar: {
-                showQuickFilter: true,
-              },
-            }}
-            onRowClick={({ row }) => {
-              sendData("selectedVendorItem", row);
-              setOpenVendorItem(false);
-            }}
-          />
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "end",
-            padding: "1em 2em 0em 2em !important",
+    <>
+      loadidng ?<div>Loading...</div>:
+      <Modal open={openVendorItem} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            minWidth: 800,
+            minHeight: 400,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
           }}
         >
-          <Button
-            variant="contained"
-            sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
-            color="primary"
-            startIcon={<CloseIcon />}
-            onClick={handleClose}
+          <Typography variant="h6">Vendor Item Modal</Typography>
+
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={vendoritemcols}
+              disableColumnFilter
+              disableColumnSelector
+              disableDensitySelector
+              slots={{ toolbar: GridToolbar }}
+              slotProps={{
+                toolbar: {
+                  showQuickFilter: true,
+                },
+              }}
+              onRowClick={({ row }) => {
+                sendData("selectedVendorItem", row);
+                setOpenVendorItem(false);
+              }}
+              pagination
+            />
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              padding: "1em 2em 0em 2em !important",
+            }}
           >
-            Close
-          </Button>
-        </div>
-      </Box>
-    </Modal>
+            <Button
+              variant="contained"
+              sx={{ width: "8.5rem", margin: "1em 0.5em !important" }}
+              color="primary"
+              startIcon={<CloseIcon />}
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </>
   );
 }
