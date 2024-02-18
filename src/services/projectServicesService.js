81e8bdd055fd6ce385820dce615efedf84f601f9
sCodeu <sachindu99u@gmail.com>
2024-02-19 03:17:41 +0530

    Created ProjectServices Service

---
+++ src/services/projectServicesService.js
@@ -0,0 +1,41 @@
+import { get, post, put, del } from '../api/axios'
+const SERVICE_URL = '/ProjectService'
+
+export async function listService(projectId, page, itemsPerPage) {
+    const services = [];
+
+    try {
+        const response = await get(`${SERVICE_URL}`, { projectId, page, pageSize: itemsPerPage });
+        response?.data?.forEach((service) => {
+            servicess.push(service)
+        })
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return services;
+}
+
+export async function getService(id) {
+    let service = {};
+
+    try {
+        const response = await get(`${SERVICE_URL}/${id}`);
+        service = response?.data;
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return service;
+}
+
+export async function addService(service) {
+    try {
+        await post(`${SERVICE_URL}`, service);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
