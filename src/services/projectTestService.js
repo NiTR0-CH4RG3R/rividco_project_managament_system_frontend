dcdcf9e3ba09348d863c3e2b63c4befe8f5f6c5d
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-19 00:04:16 +0530

    Oh! created the service for Tests

--- src/services/projectTestService.js
+++ src/services/projectTestService.js
@@ -1,4 +1,42 @@
 import { get, post, put, del } from '../api/axios'
 
-const TEST_URL='/project/test'
+const TEST_URL = '/ProjectTest'
 
+export async function listTests(projectId, page, itemsPerPage) {
+    const tests = [];
+
+    try {
+        const response = await get(`${TEST_URL}`, { projectId, page, pageSize: itemsPerPage });
+        response?.data?.forEach((test) => {
+            tests.push(test)
+        })
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return tests;
+}
+
+export async function getTest(id) {
+    let test = {};
+
+    try {
+        const response = await get(`${TEST_URL}/${id}`);
+        test = response?.data;
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    return test;
+}
+
+export async function addTest(test) {
+    try {
+        await post(`${TEST_URL}`, test);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
