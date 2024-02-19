55347a621419a66e9e70f3dc9f039ad6475d1a68
sCodeu <sachindu99u@gmail.com>
2024-02-19 14:25:55 +0530

    Change Close btn

--- src/Components/ModalWindow/VendorModal.jsx
+++ src/Components/ModalWindow/VendorModal.jsx
@@ -4,82 +4,94 @@ import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import * as vendorService from "../../services/vendorService";
 import CloseIcon from "@mui/icons-material/Close";
+import { IconButton } from "@mui/material";
 
 export default function VendorModal(props) {
-    const { openVendor, setOpenVendor, sendData } = props;
+  const { openVendor, setOpenVendor, sendData } = props;
 
-    const vendorcols = [
-        { field: "name", headerName: "Name", align: "left" },
-        { field: "address", headerName: "Address", align: "left" },
-        { field: "email", headerName: "Email", align: "left" },
+  const vendorcols = [
+    { field: "name", headerName: "Name", align: "left" },
+    { field: "address", headerName: "Address", align: "left" },
+    { field: "email", headerName: "Email", align: "left" },
 
-        { field: "phone01", headerName: "Contact Number" },
-    ];
+    { field: "phone01", headerName: "Contact Number" },
+  ];
 
-    const handleClose = () => {
-        setOpenVendor(false);
-    };
+  const handleClose = () => {
+    setOpenVendor(false);
+  };
 
-    const [rows, setRows] = useState([]);
-    const [loadidng, setLoading] = useState(false);
+  const [rows, setRows] = useState([]);
+  const [loadidng, setLoading] = useState(false);
 
-    useEffect(() => {
-        setLoading(true);
-        vendorService
-            .listVendors(1, 100)
-            .then((vendor) => {
-                setRows(vendor);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error(error);
-                setLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    setLoading(true);
+    vendorService
+      .listVendors(1, 100)
+      .then((vendor) => {
+        setRows(vendor);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-        <>
-            {loadidng ? (
-                <div>Loading...</div>
-            ) : (
-                <Modal open={openVendor} onClose={handleClose}>
-                    <Box
-                        sx={{
-                            position: "absolute",
-                            top: "50%",
-                            left: "50%",
-                            transform: "translate(-50%, -50%)",
-                            minWidth: 800,
-                            minHeight: 400,
-                            bgcolor: "background.paper",
-                            boxShadow: 24,
-                            p: 4,
-                        }}
-                    >
-                        <Typography variant="h6">Vendor Modal</Typography>
+  return (
+    <>
+      {loadidng ? (
+        <div>Loading...</div>
+      ) : (
+        <Modal open={openVendor} onClose={handleClose}>
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              minWidth: 800,
+              minHeight: 400,
+              bgcolor: "background.paper",
+              boxShadow: 24,
+              p: 4,
+            }}
+          >
+            <Typography variant="h6">Vendor Modal</Typography>
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                position: "absolute",
+                right: 8,
+                top: 8,
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
 
-                        <div style={{ height: 400, width: "100%" }}>
-                            <DataGrid
-                                rows={rows}
-                                columns={vendorcols}
-                                disableColumnFilter
-                                disableColumnSelector
-                                disableDensitySelector
-                                slots={{ toolbar: GridToolbar }}
-                                slotProps={{
-                                    toolbar: {
-                                        showQuickFilter: true,
-                                    },
-                                }}
-                                onRowClick={({ row }) => {
-                                    sendData("selectedVendor", row);
-                                    setOpenVendor(false);
-                                }}
-                                pagination
-                            />
-                        </div>
+            <div style={{ height: 400, width: "100%" }}>
+              <DataGrid
+                rows={rows}
+                columns={vendorcols}
+                disableColumnFilter
+                disableColumnSelector
+                disableDensitySelector
+                slots={{ toolbar: GridToolbar }}
+                slotProps={{
+                  toolbar: {
+                    showQuickFilter: true,
+                  },
+                }}
+                onRowClick={({ row }) => {
+                  sendData("selectedVendor", row);
+                  setOpenVendor(false);
+                }}
+                pagination
+              />
+            </div>
 
-                        <div
+            {/* <div
                             style={{
                                 display: "flex",
                                 justifyContent: "end",
@@ -95,10 +107,10 @@ export default function VendorModal(props) {
                             >
                                 Close
                             </Button>
-                        </div>
-                    </Box>
-                </Modal>
-            )}
-        </>
-    );
+                        </div> */}
+          </Box>
+        </Modal>
+      )}
+    </>
+  );
 }
