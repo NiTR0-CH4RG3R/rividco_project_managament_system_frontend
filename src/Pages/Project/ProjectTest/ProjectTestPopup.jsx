cd2e2438e37f5b46301634d9537e48e6072f63c4
sCodeu <sachindu99u@gmail.com>
2024-02-19 14:38:30 +0530

    Change Close btn

--- src/Pages/Project/ProjectTest/ProjectTestPopup.jsx
+++ src/Pages/Project/ProjectTest/ProjectTestPopup.jsx
@@ -1,11 +1,27 @@
 import { Dialog, DialogContent, DialogTitle } from "@mui/material";
 import React from "react";
 import ProjectTestForm from "./ProjectTestForm";
+import { IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function ProjectTestPopup(props) {
   const { openPopUp, setOpenPopup, testId, type } = props;
+  const handleClose = () => {
+    setOpenPopup(false);
+  };
   return (
     <Dialog open={openPopUp} onClose={() => setOpenPopup(false)}>
+      <IconButton
+        aria-label="close"
+        onClick={handleClose}
+        sx={{
+          position: "absolute",
+          right: 8,
+          top: 8,
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
       <DialogTitle>
         {testId === null ? <>Add New Test</> : <>Show Test</>}
       </DialogTitle>
