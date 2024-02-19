cbede70e44461e6e5e86b979d747c8c16d41b175
Yashod Rathnayake <144260555+YashodR@users.noreply.github.com>
2024-02-19 16:35:04 +0530

    added close button for project services

--- src/Pages/Project/ProjectServices/ProjectServicesPopup.jsx
+++ src/Pages/Project/ProjectServices/ProjectServicesPopup.jsx
@@ -1,13 +1,31 @@
 import { Dialog, DialogContent, DialogTitle } from '@mui/material'
 import React from 'react'
 import ProjectServicesForm from './ProjectServicesForm'
+import CloseIcon from '@mui/icons-material/Close'
+import IconButton from '@mui/material/IconButton'
 
 export default function ProjectServicesPopup(props) {
   const { openPopUp, setOpenPopup,type } = props
+  const handleClose = () => {
+    setOpenPopup(false);
+  };
+
   return (
     <Dialog open={openPopUp} onClose={() => setOpenPopup(false)}>
       <DialogTitle>
         Add New Service</DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,            
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+
       <DialogContent>
         <ProjectServicesForm type={type} />
       </DialogContent>
