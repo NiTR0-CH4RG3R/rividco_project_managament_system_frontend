71175721715c1d58538038bb3dec5888e940ce28
Yashod Rathnayake <144260555+YashodR@users.noreply.github.com>
2024-02-19 16:34:38 +0530

    added close button for project items

--- src/Pages/Project/ProjectItems/ProjectItemsPopup.jsx
+++ src/Pages/Project/ProjectItems/ProjectItemsPopup.jsx
@@ -1,15 +1,33 @@
 import { Dialog, DialogContent, DialogTitle } from '@mui/material'
 import React from 'react'
 import ProjectItemsForm from './ProjectItemsForm'
+import CloseIcon from '@mui/icons-material/Close'
+import IconButton from '@mui/material/IconButton'
 
 export default function ProjectItemsPopup(props) {
   const { openPopUp, setOpenPopup, type } = props
+  const handleClose = () => {
+    setOpenPopup(false);
+  };
+
   return (
     <Dialog open={openPopUp} onClose={() => setOpenPopup(false)}>
       <DialogTitle>Add New Item</DialogTitle>
+      <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
       <DialogContent>
         <ProjectItemsForm type={type} />
       </DialogContent>
     </Dialog>
   )
 }
+
