569b9357ddb6c66001739d44a93e8614d8a799de
Yashod Rathnayake <144260555+YashodR@users.noreply.github.com>
2024-02-19 17:05:49 +0530

    Add close button to the task popup

--- src/Pages/CIA/TaskStatus/TaskStatusPopup.jsx
+++ src/Pages/CIA/TaskStatus/TaskStatusPopup.jsx
@@ -1,12 +1,30 @@
 import { Dialog, DialogContent, DialogTitle } from '@mui/material'
 import React from 'react'
 import TaskForm from './TaskForm'
+import CloseIcon from '@mui/icons-material/Close'
+import IconButton from '@mui/material/IconButton'
 
 export default function TaskStatusPopup(props) {
   const { openPopUp, setOpenPopup } = props
+  const handleClose = () => {
+    setOpenPopup(false);
+  };
+
   return (
     <Dialog open={openPopUp} onClose={() => setOpenPopup(false)}>
       <DialogTitle>Add New Task Status</DialogTitle>
+      <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+
       <DialogContent>
         <TaskForm setOpenPopup={setOpenPopup} />
       </DialogContent>
