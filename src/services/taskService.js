e84a183862de4adc4dfe0beb0bd2ae223134e514
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-20 17:18:08 +0530

    taskServices pages added returns to functions

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -3,58 +3,59 @@ import { get, post, put, del } from '../api/axios'
 const TASK_URL = '/Task'
 
 export async function listTasks(page, itemsPerPage) {
-    const tasks = [];
+  const tasks = []
 
-    try {
-        const response = await get(TASK_URL, { page, pageSize: itemsPerPage });
-        response?.data?.forEach((task) => {
-            tasks.push(task)
-        })
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await get(TASK_URL, { page, pageSize: itemsPerPage })
+    response?.data?.forEach((task) => {
+      tasks.push(task)
+    })
+  } catch (error) {
+    console.error(error)
+  }
 
-    return tasks;
+  return tasks
 }
 
 export async function getTask(id) {
-    let task = {};
+  let task = {}
 
-    try {
-        const response = await get(`${TASK_URL}/${id}`);
-        task = response?.data;
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await get(`${TASK_URL}/${id}`)
+    task = response?.data
+  } catch (error) {
+    console.error(error)
+  }
 
-    return task;
+  return task
 }
 
 export async function addTask(task) {
-    try {
-        await post(TASK_URL, task);
-    }
-    catch (error) {
-        console.error(error);
-    }
+  let addedtask = {}
+  try {
+    const response = await post(TASK_URL, task)
+    addedtask = response?.data
+  } catch (error) {
+    console.error(error)
+  }
+  return addedtask
 }
 
 export async function updateTask(task, id) {
-    try {
-        await put(`${TASK_URL}/${id}`, task);
-    }
-    catch (error) {
-        console.error(error);
-    }
+  let updatedtask = {}
+  try {
+    const response = await put(`${TASK_URL}/${id}`, task)
+    updatedtask = response?.data
+  } catch (error) {
+    console.error(error)
+  }
+  return updatedtask
 }
 
 export async function deleteTask(id) {
-    try {
-        await del(`${TASK_URL}/${id}`);
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    await del(`${TASK_URL}/${id}`)
+  } catch (error) {
+    console.error(error)
+  }
 }
