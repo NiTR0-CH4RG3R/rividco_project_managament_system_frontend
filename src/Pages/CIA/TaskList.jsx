2f0269fdde75413e6910b52bf938b2cb575e2c99
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-20 17:26:39 +0530

    changed variable names as backend in task list

--- src/Pages/CIA/TaskList.jsx
+++ src/Pages/CIA/TaskList.jsx
@@ -1,75 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTopbarContext } from '../../Contexts/TopbarContext';
-import ListPage from '../../Components/ListPage/ListPage';
-import { AppRoutes } from '../../Data/AppRoutes';
-import * as taskService from '../../services/taskService';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useTopbarContext } from '../../Contexts/TopbarContext'
+import ListPage from '../../Components/ListPage/ListPage'
+import { AppRoutes } from '../../Data/AppRoutes'
+import * as taskService from '../../services/taskService'
+import * as taskStatusService from '../../services/taskStatusService'
 
 const columns = [
-    { id: 'id', label: 'Id', align: 'left' },
-    { id: 'category', label: 'Category', align: 'left' },
-    { id: 'requestedBy', label: 'Requested By', align: 'left' },
-    { id: 'callBackNumber', label: 'Callback No.', align: 'left' },
-    { id: 'status', label: 'Status', align: 'center' },
-    { id: 'assignedTo', label: 'Assigned To', align: 'left' },
-];
+  { id: 'id', label: 'Id', align: 'left' },
+  { id: 'category', label: 'Category', align: 'left' },
+  { id: 'requestedBy', label: 'Requested By', align: 'left' },
+  { id: 'callBackNumber', label: 'Callback No.', align: 'left' },
+  { id: 'status', label: 'Status', align: 'center' },
+  { id: 'assignedTo', label: 'Assigned To', align: 'left' },
+]
 
 export default function TaskList() {
-    const { setTitle, setSubtitle } = useTopbarContext();
-    setTitle("CIA Task Projects");
-    setSubtitle("You can view and manage all the projects here.");
+  const { setTitle, setSubtitle } = useTopbarContext()
+  setTitle('CIA Task Projects')
+  setSubtitle('You can view and manage all the projects here.')
 
-    const [rows, setRows] = useState([
-        { id: 0, category: 'Complaint', requestedBy: 'Jane Doe', callback: '091 2222222', assignedTo: 'Jone Doe', status: 'ACTIVE', },
-    ]);
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rows, setRows] = useState([
+    {
+      id: 0,
+      category: 'Complaint',
+      requestedBy: 'Jane Doe',
+      callback: '091 2222222',
+      assignedTo: 'Jone Doe',
+      status: 'ACTIVE',
+    },
+  ])
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
 
-    useEffect(() => {
-        setPage(0);
-        setRowsPerPage(5);
-    }, []);
+  useEffect(() => {
+    setPage(0)
+    setRowsPerPage(5)
+  }, [])
 
-    useEffect(() => {
-        taskService.listTasks(page + 1, rowsPerPage)
-            .then(task => {
-                setRows(task);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, [page, rowsPerPage]);
+  useEffect(() => {
+    taskService.listTasks(page + 1, rowsPerPage).then((task) => {
+      setRows(task)
+    })
+    taskStatusService
+      .listTaskStatus(page + 1, rowsPerPage)
+      .then((taskstatuses) => {
+        setRows(taskstatuses)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [page, rowsPerPage])
 
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-
-
-    return (
-        <ListPage
-            columns={columns}
-            rows={rows}
-            searchBarProps={{
-                searchBy: 'location',
-                onSearchChange: (e) => { console.log(e.target.value); },
-                onSearchClick: (e) => { },
-            }}
-            onRowClick={(e, id) => {
-                navigate(AppRoutes.cia_view.path.replace(':id', id));
-            }}
-            onAddButtonClick={(e) => {
-                navigate(AppRoutes.cia_add.path)
-            }}
-            tablePaginationProps={{
-                rowsPerPageOptions: [5, 10, 25, 100],
-                component: "div",
-                rowsPerPage: rowsPerPage,
-                page: page,
-                count: -1,
-                onPageChange: (e, page) => { setPage(page); },
-                onRowsPerPageChange: (e) => { setRowsPerPage(e.target.value); }
-            }}
-            disableSearchBar
-        // customUpperBar={<UpperBar />}
-        />
-    );
+  return (
+    <ListPage
+      columns={columns}
+      rows={rows}
+      searchBarProps={{
+        searchBy: 'location',
+        onSearchChange: (e) => {
+          console.log(e.target.value)
+        },
+        onSearchClick: (e) => {},
+      }}
+      onRowClick={(e, id) => {
+        navigate(AppRoutes.cia_view.path.replace(':id', id))
+      }}
+      onAddButtonClick={(e) => {
+        navigate(AppRoutes.cia_add.path)
+      }}
+      tablePaginationProps={{
+        rowsPerPageOptions: [5, 10, 25, 100],
+        component: 'div',
+        rowsPerPage: rowsPerPage,
+        page: page,
+        count: -1,
+        onPageChange: (e, page) => {
+          setPage(page)
+        },
+        onRowsPerPageChange: (e) => {
+          setRowsPerPage(e.target.value)
+        },
+      }}
+      disableSearchBar
+      // customUpperBar={<UpperBar />}
+    />
+  )
 }
