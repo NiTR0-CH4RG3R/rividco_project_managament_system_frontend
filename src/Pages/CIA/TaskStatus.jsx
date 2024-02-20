8225d80cdb6ed73bc1989da096a7ed8d44a4efba
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-02-20 17:27:02 +0530

    Added use effect to task status page

--- src/Pages/CIA/TaskStatus.jsx
+++ src/Pages/CIA/TaskStatus.jsx
@@ -2,30 +2,53 @@ import * as React from 'react'
 import { useTopbarContext } from '../../Contexts/TopbarContext'
 import TaskStatusPopup from './TaskStatus/TaskStatusPopup'
 import ListPage from '../../Components/ListPage/ListPage'
+import * as taskStatusService from '../../services/taskStatusService'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function TaskStatus() {
   const { setTitle, setSubtitle } = useTopbarContext()
   setTitle('List CIA Task Status')
   setSubtitle('You can view and manage all the List CIA Task Status here.')
 
+  const [openPopUp, setOpenPopup] = React.useState(false)
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
+  const { id } = useParams()
+
   const columns = [
     { id: 'status', label: 'Task Status', align: 'left' },
-    { id: 'lastupdatedby', label: 'Last updated By', align: 'left' },
-    { id: 'lastupdateddate', label: 'Lastupdated Date', align: 'left' },
-    { id: 'comment', label: 'Comment', align: 'left' },
+    { id: 'lastUpdatedBy', label: 'Last updated By', align: 'left' },
+    { id: 'lastUpdatedDate', label: 'Lastupdated Date', align: 'left' },
+    { id: 'comments', label: 'Comment', align: 'left' },
   ]
 
   const [rows, setRows] = React.useState([
     {
-      id: 1,
       status: 'Active',
-      lastupdatedby: 'john silva',
-      lastupdateddate: '2024-01-30',
-      comment: 'This task is in active status',
+      lastUpdatedBy: 'john silva',
+      lastUpdatedDateTime: '2024-01-30',
+      comments: 'This task is in active status',
     },
   ])
 
-  const [openPopUp, setOpenPopup] = React.useState(false)
+  useEffect(() => {
+    setPage(0)
+    setRowsPerPage(5)
+  }, [])
+
+  useEffect(() => {
+    taskStatusService
+      .listTaskStatus(page + 1, rowsPerPage)
+      .then((taskstatuses) => {
+        setRows(taskstatuses)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [id, page, rowsPerPage])
+
   return (
     <>
       <ListPage
@@ -47,14 +70,14 @@ function TaskStatus() {
         tablePaginationProps={{
           rowsPerPageOptions: [5, 10, 25, 100],
           component: 'div',
-          rowsPerPage: 5,
-          page: 0,
-          count: 100,
+          rowsPerPage: rowsPerPage,
+          page: page,
+          count: -1,
           onPageChange: (e, page) => {
-            console.log(page)
+            setPage(page)
           },
           onRowsPerPageChange: (e) => {
-            console.log(e.target.value)
+            setRowsPerPage(e.target.value)
           },
         }}
         disableSearchBar
