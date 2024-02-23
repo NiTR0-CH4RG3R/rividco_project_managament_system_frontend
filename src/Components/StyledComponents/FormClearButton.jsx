b389678003a2f974acbb87d25640012f662956f4
Punsara Weerasinghe <weerasingha11744@usci.ruh.ac.lk>
2024-02-23 14:52:27 +0530

    customize Clear button component

--- src/Components/StyledComponents/FormClearButton.jsx
+++ src/Components/StyledComponents/FormClearButton.jsx
@@ -1,8 +1,22 @@
 import { Button } from "@mui/material";
-import { styled, alpha } from "@mui/system";
+import styled from "styled-components";
 
+// const FormClearButton = styled(Button)(({ theme }) => ({
+//   //add styles here
+// }));
+// export default FormClearButton;
 
-const FormClearButton = styled(Button)(({ theme }) => ({
-  //add styles here
-}));
+const FormClearButton = styled.button`
+  border: 2px solid #00025d;
+  background-color: #19328b;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.5s all ease-out;
+`;
+
 export default FormClearButton;
