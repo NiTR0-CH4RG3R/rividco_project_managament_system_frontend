8ddd202c2508c16862872fa2d1bccd21789e411f
Sanjana Amarasinghe <120704911+Nalajitha99@users.noreply.github.com>
2024-02-26 13:25:14 +0530

    Task Resource services added

--- src/services/taskResourceService.js
+++ src/services/taskResourceService.js
@@ -22,3 +22,28 @@ export async function listTaskResourceService(taskId , page, itemsPerPage) {
     return taskResource
 }
 
+export async function getTaskResource(id) {
+    let taskresource = {}
+
+    try {
+        const response = await get(`${TASKRESOURCE_URL}/${id}`)
+        taskresource = response?.data
+    } catch {
+        console.error(error)
+    }
+
+    return taskresource
+}
+
+export async function addTaskResource(taskResorce) {
+    let addedtaskresource = {}
+
+    try {
+        const response = await post(`${TASKRESOURCE_URL}`, taskResorce)
+        addedtaskresource = response?.data
+    } catch {
+        console.error(error)
+    }
+
+    return addedtaskresource
+}
