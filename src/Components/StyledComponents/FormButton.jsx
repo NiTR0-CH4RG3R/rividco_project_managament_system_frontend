787fc30dd06c0a10ef26c76b2d13b399dad3adff
sCodeu <sachindu99u@gmail.com>
2024-03-02 02:47:11 +0530

    Added styled for FormButton

--- src/Components/StyledComponents/FormButton.jsx
+++ src/Components/StyledComponents/FormButton.jsx
@@ -3,6 +3,31 @@ import { styled, alpha } from "@mui/system";
 
 
 const FormButton = styled(Button)(({ theme }) => ({
-  //add styles here
+  boxShadow: "-moz-initial",
+  textTransform: "none",
+  fontSize: 16,
+  padding: "6px 12px",
+  border: "1px solid",
+  lineHeight: 1.5,
+  backgroundColor: "#073459",
+  borderColor: "#2e9af2",
+  borderRadius:"8px",
+  
+
+  "&:hover": {
+    backgroundColor: "#0a538f",
+    borderColor: "#2e9af2",
+    boxShadow: "none",
+    color:"white",
+    transform: "scale(1.1)",
+  },
+  "&:active": {
+    boxShadow: "none",
+    backgroundColor: "#0a538f",
+    borderColor: "#2e9af2",
+  },
+  "&:focus": {
+    //boxShadow: "0 0 0 0.2rem rgba(255, 0, 0, 0.5)",
+  },
 }));
 export default FormButton;
