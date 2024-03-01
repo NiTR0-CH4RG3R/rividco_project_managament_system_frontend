0b04535525307597270fe8cc03c45a4362816213
Punsara Weerasinghe <weerasingha11744@usci.ruh.ac.lk>
2024-03-01 16:34:06 +0530

    Added Tooltip for Floting icon button in ProjectList page

--- src/Pages/Project/ProjectList.jsx
+++ src/Pages/Project/ProjectList.jsx
@@ -6,80 +6,81 @@ import { AppRoutes } from "../../Data/AppRoutes";
 import * as projectService from "../../services/projectService";
 
 const columns = [
-    { id: "projectIdentificationNumber", label: "First Name", align: "left" },
-    { id: "customerId", label: "Customer", align: "left" },
-    { id: "address", label: "Location", align: "left" },
-    { id: "salesPerson", label: "Sales Person", align: "left" },
-    { id: "status", label: "Status", align: "center" },
+  { id: "projectIdentificationNumber", label: "First Name", align: "left" },
+  { id: "customerId", label: "Customer", align: "left" },
+  { id: "address", label: "Location", align: "left" },
+  { id: "salesPerson", label: "Sales Person", align: "left" },
+  { id: "status", label: "Status", align: "center" },
 ];
 
 export default function ProjectList() {
-    const { setTitle, setSubtitle } = useTopbarContext();
-    setTitle("List Projects");
-    setSubtitle("You can view and manage all the projects here.");
+  const { setTitle, setSubtitle } = useTopbarContext();
+  setTitle("List Projects");
+  setSubtitle("You can view and manage all the projects here.");
 
-    const [rows, setRows] = useState([
-        {
-            id: 0,
-            identificationNo: "P001",
-            customer: "Jane Doe",
-            location: "No. 380, Walawwaththa, Dadalla, Galle",
-            salesPerson: "Jone Doe",
-            status: "ACTIVE",
+  const [rows, setRows] = useState([
+    {
+      id: 0,
+      identificationNo: "P001",
+      customer: "Jane Doe",
+      location: "No. 380, Walawwaththa, Dadalla, Galle",
+      salesPerson: "Jone Doe",
+      status: "ACTIVE",
+    },
+  ]);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    setPage(0);
+    setRowsPerPage(5);
+  }, []);
+
+  useEffect(() => {
+    projectService
+      .listProjects(page + 1, rowsPerPage)
+      .then((projects) => {
+        setRows(projects);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [page, setRows]);
+
+  const navigate = useNavigate();
+
+  return (
+    <ListPage
+      tiptitle={"Add New Project"}
+      columns={columns}
+      rows={rows}
+      searchBarProps={{
+        searchBy: "location",
+        onSearchChange: (e) => {
+          console.log(e.target.value);
         },
-    ]);
-
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
-
-    useEffect(() => {
-        setPage(0);
-        setRowsPerPage(5);
-    }, []);
-
-    useEffect(() => {
-        projectService
-            .listProjects(page + 1, rowsPerPage)
-            .then((projects) => {
-                setRows(projects);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, [page, setRows]);
-
-    const navigate = useNavigate();
-
-    return (
-        <ListPage
-            columns={columns}
-            rows={rows}
-            searchBarProps={{
-                searchBy: "location",
-                onSearchChange: (e) => {
-                    console.log(e.target.value);
-                },
-                onSearchClick: (e) => { },
-            }}
-            onRowClick={(e, id) => {
-                console.log(id);
-            }}
-            onAddButtonClick={(e) => {
-                navigate(AppRoutes.project_add.path);
-            }}
-            tablePaginationProps={{
-                rowsPerPageOptions: [5, 10, 25, 100],
-                component: "div",
-                rowsPerPage: 5,
-                page: 0,
-                count: 100,
-                onPageChange: (e, page) => {
-                    setPage(page);
-                },
-                onRowsPerPageChange: (e) => {
-                    setRowsPerPage(e.target.value);
-                },
-            }}
-        />
-    );
+        onSearchClick: (e) => {},
+      }}
+      onRowClick={(e, id) => {
+        console.log(id);
+      }}
+      onAddButtonClick={(e) => {
+        navigate(AppRoutes.project_add.path);
+      }}
+      tablePaginationProps={{
+        rowsPerPageOptions: [5, 10, 25, 100],
+        component: "div",
+        rowsPerPage: 5,
+        page: 0,
+        count: 100,
+        onPageChange: (e, page) => {
+          setPage(page);
+        },
+        onRowsPerPageChange: (e) => {
+          setRowsPerPage(e.target.value);
+        },
+      }}
+    />
+  );
 }
