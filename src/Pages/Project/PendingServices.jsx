d07c31e5189b0b3df650d78b4e250fe816b4ed62
Punsara Weerasinghe <weerasingha11744@usci.ruh.ac.lk>
2024-03-01 16:33:26 +0530

    Added Tooltip for Floting icon button in pending service page

--- src/Pages/Project/PendingServices.jsx
+++ src/Pages/Project/PendingServices.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useTopbarContext } from "../../Contexts/TopbarContext";
 import ListPage from "../../Components/ListPage/ListPage";
 import { useNavigate } from "react-router-dom";
-import ProjectServicesPopup from '../Project/ProjectServices/ProjectServicesPopup'
-
+import ProjectServicesPopup from "../Project/ProjectServices/ProjectServicesPopup";
 
 const columns = [
   { id: "description", label: "Description", align: "left" },
@@ -11,25 +10,23 @@ const columns = [
   { id: "conductedBy", label: "Conducted By ", align: "left" },
   { id: "conductedDate", label: "Conducted Date", align: "center" },
   { id: "dueDate", label: "Due Date", align: "center" },
-
 ];
 
 export default function PendingServices() {
-        const { setTitle, setSubtitle } = useTopbarContext();
-        setTitle("Pending services");
-        setSubtitle("You can view and manage all the pending services here.");
-    
-  const [openPopUp, setOpenPopup] = useState(false);
-  const[modeType,setModeType]=useState();
+  const { setTitle, setSubtitle } = useTopbarContext();
+  setTitle("Pending services");
+  setSubtitle("You can view and manage all the pending services here.");
 
+  const [openPopUp, setOpenPopup] = useState(false);
+  const [modeType, setModeType] = useState();
 
   const [rows, setRows] = useState([
     {
-        description: "description 1",
-        status: "pending",
-        conductedBy: "user 1",
-        conductedDate: "02/01/2024",
-        dueDate: "02/01/2024",
+      description: "description 1",
+      status: "pending",
+      conductedBy: "user 1",
+      conductedDate: "02/01/2024",
+      dueDate: "02/01/2024",
     },
   ]);
 
@@ -38,6 +35,7 @@ export default function PendingServices() {
   return (
     <>
       <ListPage
+        tiptitle={"Add New Pending Service"}
         columns={columns}
         rows={rows}
         searchBarProps={{
@@ -50,8 +48,8 @@ export default function PendingServices() {
         onRowClick={(e, id) => {
           setModeType("view");
           setOpenPopup(true);
-      }}
-      onAddButtonClick={(e) => {
+        }}
+        onAddButtonClick={(e) => {
           setModeType("add");
           setOpenPopup(true);
         }}
@@ -71,8 +69,11 @@ export default function PendingServices() {
         disableSearchBar
         // customUpperBar={<UpperBar />}
       />
-      <ProjectServicesPopup openPopUp={openPopUp} setOpenPopup={setOpenPopup} type={modeType} />
+      <ProjectServicesPopup
+        openPopUp={openPopUp}
+        setOpenPopup={setOpenPopup}
+        type={modeType}
+      />
     </>
   );
 }
-
