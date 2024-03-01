23c6fa610c804cefe1c12f96a786b5ea9c9599bc
Punsara Weerasinghe <weerasingha11744@usci.ruh.ac.lk>
2024-03-01 13:23:47 +0530

    Update the page

--- src/Components/SideNavigationPanel/SideNavigationPanelMenu.jsx
+++ src/Components/SideNavigationPanel/SideNavigationPanelMenu.jsx
@@ -1,59 +1,60 @@
-import { useState } from 'react';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useState } from "react";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { useNavigate, useLocation } from "react-router-dom";
 import NavMenuitemListitemButton from "../../Components/StyledComponents/NavMenuItemListitemButton";
 import NavSubItemListItemButton from "../../Components/StyledComponents/NavSubItemListItemButton";
 
 export default function SideNavigationPanelMenu({ name, icon, subMenu }) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const handleClick = () => {
-        setOpen(!open);
-    };
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
-    return (
-        <>
-            <NavMenuitemListitemButton onClick={handleClick} selected={open}>
-                <ListItemIcon>
-                    {icon}
-                </ListItemIcon>
-                <ListItemText primary={name} />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </NavMenuitemListitemButton>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    {
-                        subMenu.map((subItem) => (
-                            <NavSubItemListItemButton key={subItem.name} onClick={() => (navigate(subItem.path))} selected={location.pathname === subItem.path}>
-                                <ListItemText
-                                    primaryTypographyProps={{
-                                        variant: 'body2',
-                                        sx: {
-                                            display: 'flex',
-                                            justifyContent: 'flex-end'
-                                        }
-                                    }}
-                                    secondaryTypographyProps={{
-                                        sx: {
-                                            display: 'flex',
-                                            justifyContent: 'flex-end'
-                                        }
-                                    }}
-                                    primary={subItem.name} />
-                            </NavSubItemListItemButton>
-                        ))
-                    }
-                </List>
-            </Collapse>
-        </>
-    );
+  return (
+    <>
+      <NavMenuitemListitemButton onClick={handleClick} selected={open}>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary={name} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </NavMenuitemListitemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {subMenu.map((subItem) => (
+            <NavSubItemListItemButton
+              key={subItem.name}
+              onClick={() => navigate(subItem.path)}
+              selected={location.pathname === subItem.path}
+            >
+              <ListItemText
+                primaryTypographyProps={{
+                  variant: "body2",
+                  sx: {
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  },
+                }}
+                secondaryTypographyProps={{
+                  sx: {
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  },
+                }}
+                primary={subItem.name}
+              />
+            </NavSubItemListItemButton>
+          ))}
+        </List>
+      </Collapse>
+    </>
+  );
 }
