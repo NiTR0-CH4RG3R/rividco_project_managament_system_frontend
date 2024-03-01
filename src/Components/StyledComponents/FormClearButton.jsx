511cafec611791c1cc3008e1ae63447e07be882d
sCodeu <sachindu99u@gmail.com>
2024-03-02 01:25:25 +0530

    Change Clear Button

--- src/Components/StyledComponents/FormClearButton.jsx
+++ src/Components/StyledComponents/FormClearButton.jsx
@@ -8,21 +8,24 @@ const FormClearButton = styled(Button)(({ theme }) => ({
   padding: "6px 12px",
   border: "1px solid",
   lineHeight: 1.5,
-  backgroundColor: "#0063cc",
-  borderColor: "#0063cc",
+  //backgroundColor: "#0063cc",
+  borderColor: "#c70000",
+  color:"#c70000",
 
   "&:hover": {
-    backgroundColor: "#0069d9",
-    borderColor: "#0062cc",
+    backgroundColor: "#c70000",
+    borderColor: "#c70000",
     boxShadow: "none",
+    color:"white",
+    transform: "scale(1.1)",
   },
   "&:active": {
     boxShadow: "none",
-    backgroundColor: "#0062cc",
-    borderColor: "#005cbf",
+    backgroundColor: "#c70000",
+    borderColor: "##c70000",
   },
   "&:focus": {
-    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+    //boxShadow: "0 0 0 0.2rem rgba(255, 0, 0, 0.5)",
   },
 }));
 export default FormClearButton;
