39d7d878c4d76374969e7c5761f261fc76c86cdb
sCodeu <sachindu99u@gmail.com>
2024-03-02 00:07:39 +0530

    Set hover border radius

--- src/Components/SideNavigationPanel/SideNavigationPanelMenu.jsx
+++ src/Components/SideNavigationPanel/SideNavigationPanelMenu.jsx
@@ -36,7 +36,8 @@ export default function SideNavigationPanelMenu({ name, icon, subMenu }) {
               selected={location.pathname === subItem.path}
               sx={{
                 "&:hover": {
-                  backgroundColor: "#062fc2", // Change this to your desired hover color
+                  backgroundColor: "#062fc2",
+                  borderRadius: "8px",
                 },
                 
               }}
