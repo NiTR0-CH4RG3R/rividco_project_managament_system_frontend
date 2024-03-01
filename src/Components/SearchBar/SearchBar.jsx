975bb12f65488223c26162ba9395951cc416b533
sCodeu <sachindu99u@gmail.com>
2024-03-01 13:16:20 +0530

    Change size and and radius of the searchbar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -12,8 +12,9 @@ export default function SearchBar(
         <Box
             component={Paper}
             display="flex"
-            borderRadius="3px"
+            borderRadius="8px"
             p={0.5}
+            width="80%"
         >
             <InputBase sx={{ ml: 2, flex: 1 }} onChange={onSearchChange} placeholder={`Search by ${searchBy}`} />
             <IconButton type="button" onClick={onSearchClick} sx={{ p: 1 }}>
