38e0773df331f6f8862abe39c83a8c9ace4533c4
sCodeu <sachindu99u@gmail.com>
2024-03-02 12:31:36 +0530

    Added styled for the Styled Edit btn

--- src/Components/StyledComponents/FormEditButton.jsx
+++ src/Components/StyledComponents/FormEditButton.jsx
@@ -2,7 +2,32 @@ import styled from '@emotion/styled'
 import { Button } from '@mui/material'
 
 const FormEditButton = styled(Button)(({ theme }) => ({
-  //style form edit button here
+  boxShadow: "-moz-initial",
+  textTransform: "none",
+  fontSize: 16,
+  padding: "6px 12px",
+  border: "1px solid",
+  lineHeight: 1.5,
+  backgroundColor: "#071024",
+  borderColor: "#02226A",
+  borderRadius:"8px",
+  
+
+  "&:hover": {
+    backgroundColor: "#0a538f",
+    borderColor: "#2e9af2",
+    boxShadow: "none",
+    color:"white",
+    transform: "scale(1.1)",
+  },
+  "&:active": {
+    boxShadow: "none",
+    backgroundColor: "#0a538f",
+    borderColor: "#2e9af2",
+  },
+  "&:focus": {
+    //boxShadow: "0 0 0 0.2rem rgba(255, 0, 0, 0.5)",
+  },
 }))
 
 export default FormEditButton
