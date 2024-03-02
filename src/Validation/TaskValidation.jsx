a5f7901bf52ca96c9156566e65bdccae3f341dbc
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-03-02 15:24:33 +0530

    task callbacknumber validations changed

--- src/Validation/TaskValidation.jsx
+++ src/Validation/TaskValidation.jsx
@@ -4,11 +4,17 @@ export const taskValidation = yup.object().shape({
   description: yup.string().required('Required'),
   callbacknumber: yup
     .string()
-    .matches(
+    /*.matches(
       /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/,
       'Please enter a valid mobile number'
     )
     .min(10)
+    .required('Required'),*/
+    .matches(
+      /^\+\d{1,3}[0-9]{9,14}$/,
+      'Please enter a valid mobile number with country code'
+    )
+    .min(12)
     .required('Required'),
   selectedCustomer: yup.object({
     firstName: yup.string().required('Required'),
