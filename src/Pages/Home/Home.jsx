bb5a1b94d98f23f94fa1794550b51299959e6c6a
Punsara Weerasinghe <weerasingha11744@usci.ruh.ac.lk>
2024-03-11 20:19:39 +0530

    add Home page imge

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,49 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import imagePath from '../Login/blueLogoAsset 1.png';
+import React, { useEffect, useState } from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import imagePath from "../Login/solar-panel-cell-on-dramatic-sunset-sky-background-free-photo.jpg";
 import { useTopbarContext } from "../../Contexts/TopbarContext";
 
 const Home = () => {
+  const { setTitle, setSubtitle } = useTopbarContext();
+  setTitle("Home");
+  setSubtitle("Welcome to RIVIDCO PROJECTS!");
 
-    const { setTitle, setSubtitle } = useTopbarContext();
-    setTitle("Home");
-    setSubtitle("Welcome to RIVIDCO PROJECTS!");
+  const [currentTime, setDateTime] = useState(new Date());
 
-    const [currentTime, setDateTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDateTime(new Date());
-        }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
-        return () => clearInterval(intervalId);
+  return (
+    <Container>
+      <div
+        style={{
+          position: "fixed",
+          marginTop: "-21%",
+          marginLeft: "-5%",
+          width: "100vw",
+          height: "100vh",
+          opacity: "60%",
+        }}
+      >
+        <img
+          src={imagePath}
+          alt="companyLogo"
+          style={{ overflow: "hidden", width: "100%", height: "100%" }}
+        />
+      </div>
 
-    }, []);
+      <div style={{ position: "fixed", bottom: 0, right: 0, margin: "5%" }}>
+        <Typography
+          variant="h1"
+          style={{ marginBottom: "5px", fontWeight: "bold" }}
+        >
+          {currentTime.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+          })}
+        </Typography>
+        <Typography variant="h4" style={{ fontWeight: "bold" }}>
+          {currentTime.toLocaleDateString(undefined, {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </Typography>
+      </div>
+    </Container>
+  );
+};
 
-
-    return (
-        <Container>
-
-            <div style={{ position: 'fixed', top: 0, right: 0, marginTop: '10%', marginRight: '5%' }}>
-                <img
-                    src={imagePath}
-                    alt="companyLogo"
-                    style={{ width: '40vw', height: '10vw' }}
-                />
-            </div>
-
-            <div style={{ position: 'fixed', bottom: 0, right: 0, margin: '5%' }}>
-                <Typography variant="h1" style={{ marginBottom: '5px', fontWeight: 'bold' }}>
-                    {currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}
-                </Typography>
-                <Typography variant="h4" style={{ fontWeight: 'bold' }}>
-                    {currentTime.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
-                </Typography>
-            </div>
-
-        </Container>
-    )
-}
-
-export default Home
+export default Home;
