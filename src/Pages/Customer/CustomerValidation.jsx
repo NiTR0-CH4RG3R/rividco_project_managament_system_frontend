582da848de9d0b9d53df02a04c10736b2e056e26
sCodeu <sachindu99u@gmail.com>
2024-04-09 12:13:28 +0530

    Change mobile number format

--- src/Pages/Customer/CustomerValidation.jsx
+++ src/Pages/Customer/CustomerValidation.jsx
@@ -1,25 +1,27 @@
 import * as yup from "yup";
 
 export const customerValidation = yup.object().shape({
-    firstName: yup.string().required("Required"),
-    lastName: yup.string().required("Required"),
-    customerRegistrationNumber: yup.string().required("Required"),
-    address: yup.string().required("Required"),
-    email: yup.string().email("Please enter a valid email").required("Required"),
-    category: yup.string().required("Required"),
-    phone01: yup
-        .string()
-        .matches(
-            /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/,
-            "Please enter a valid mobile number"
-        )
-        .min(10)
-        .required("Required"),
-    phone02: yup
-        .string()
-        .matches(
-            /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/,
-            "Please enter a valid office number"
-        )
-        .min(10),
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("Required"),
+  customerRegistrationNumber: yup.string().required("Required"),
+  address: yup.string().required("Required"),
+  email: yup.string().email("Please enter a valid email").required("Required"),
+  category: yup.string().required("Required"),
+  phone01: yup
+    .string()
+    .matches(
+      // /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/,
+      // "Please enter a valid mobile number"
+      /^\+\d{1,3}[0-9]{9,14}$/,
+      "Please enter a valid mobile number with country code"
+    )
+    .min(10)
+    .required("Required"),
+  phone02: yup
+    .string()
+    .matches(
+      /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/,
+      "Please enter a valid office number"
+    )
+    .min(10),
 });
