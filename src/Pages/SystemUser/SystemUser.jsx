8d669cef12244a18a5288db26205c663be1842e9
sCodeu <sachindu99u@gmail.com>
2024-04-13 11:37:19 +0530

    Added catch for set values in system user

--- src/Pages/SystemUser/SystemUser.jsx
+++ src/Pages/SystemUser/SystemUser.jsx
@@ -1,118 +1,123 @@
-import React, { useState, useEffect } from 'react'
-import { Box, Grid, MenuItem, Avatar } from '@mui/material'
-import Paper from '@mui/material/Paper'
-import { useFormik } from 'formik'
-import SystemUserValidation from '../../Validation/SystemUserValidation'
-import { useParams, useNavigate } from 'react-router-dom'
-import SaveIcon from '@mui/icons-material/Save'
-import EditIcon from '@mui/icons-material/Edit'
-import ClearAllIcon from '@mui/icons-material/ClearAll'
-import { useTopbarContext } from '../../Contexts/TopbarContext'
-import FormTextField from '../../Components/StyledComponents/FormTextField'
-import FormButton from '../../Components/StyledComponents/FormButton'
-import FormClearButton from '../../Components/StyledComponents/FormClearButton'
-import FormEditButton from '../../Components/StyledComponents/FormEditButton'
-import FormSaveLoadingButton from '../../Components/StyledComponents/FormSaveLoadingButton'
-import { AppRoutes } from '../../Data/AppRoutes'
-import * as systemUserService from '../../services/systemUserService'
-import Snackbar from '@mui/material/Snackbar'
-import MuiAlert from '@mui/material/Alert'
-import Slide from '@mui/material/Slide'
+import React, { useState, useEffect } from "react";
+import { Box, Grid, MenuItem, Avatar } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { useFormik } from "formik";
+import SystemUserValidation from "../../Validation/SystemUserValidation";
+import { useParams, useNavigate } from "react-router-dom";
+import SaveIcon from "@mui/icons-material/Save";
+import EditIcon from "@mui/icons-material/Edit";
+import ClearAllIcon from "@mui/icons-material/ClearAll";
+import { useTopbarContext } from "../../Contexts/TopbarContext";
+import FormTextField from "../../Components/StyledComponents/FormTextField";
+import FormButton from "../../Components/StyledComponents/FormButton";
+import FormClearButton from "../../Components/StyledComponents/FormClearButton";
+import FormEditButton from "../../Components/StyledComponents/FormEditButton";
+import FormSaveLoadingButton from "../../Components/StyledComponents/FormSaveLoadingButton";
+import { AppRoutes } from "../../Data/AppRoutes";
+import * as systemUserService from "../../services/systemUserService";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import Slide from "@mui/material/Slide";
 
 const roles = [
   {
-    value: 'Admin',
-    label: 'Admin',
+    value: "Admin",
+    label: "Admin",
   },
   {
-    value: 'User',
-    label: 'User',
+    value: "User",
+    label: "User",
   },
-]
+];
 
 export default function SystemUser(props) {
-  const { id } = useParams()
-  const { setTitle, setSubtitle } = useTopbarContext()
+  const { id } = useParams();
+  const { setTitle, setSubtitle } = useTopbarContext();
 
   function loadSystemUserData(id, setValues) {
-    systemUserService.getSystemUser(id).then((systemUser) => {
-      const systemUserValues = {
-        firstName: systemUser.firstName,
-        lastName: systemUser.lastName,
-        address: systemUser.address,
-        email: systemUser.email,
-        role: systemUser.role,
-        mobileNo: systemUser.phone01,
-        officeNo: systemUser.phone02,
-        userName: systemUser.username,
-        password: systemUser.password,
-        comment: systemUser.comments,
-      }
-      setValues(systemUserValues)
-    })
+    systemUserService
+      .getSystemUser(id)
+      .then((systemUser) => {
+        const systemUserValues = {
+          firstName: systemUser.firstName,
+          lastName: systemUser.lastName,
+          address: systemUser.address,
+          email: systemUser.email,
+          role: systemUser.role,
+          mobileNo: systemUser.phone01,
+          officeNo: systemUser.phone02,
+          userName: systemUser.username,
+          password: systemUser.password,
+          comment: systemUser.comments,
+        };
+        setValues(systemUserValues);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   setTitle(
-    props.type === 'add'
-      ? 'Add a new System User'
-      : props.type === 'edit'
-      ? 'Edit System User'
+    props.type === "add"
+      ? "Add a new System User"
+      : props.type === "edit"
+      ? "Edit System User"
       : `View System User`
-  )
+  );
   setSubtitle(
-    props.type === 'add'
-      ? 'You can add new system user here.'
-      : props.type === 'edit'
+    props.type === "add"
+      ? "You can add new system user here."
+      : props.type === "edit"
       ? `You can edit system user id:#${id} details here.`
       : `You can view system user id:#${id} details here.`
-  )
+  );
 
   //const ProfilePictureUploader = () => {
-  const [file, setFile] = useState(null)
-  const [imagePreview, setImagePreview] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [successMessageOpen, setSuccessMessageOpen] = useState(false)
+  const [file, setFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [successMessageOpen, setSuccessMessageOpen] = useState(false);
 
   const handleCloseSuccessMessage = () => {
-    setSuccessMessageOpen(false)
-  }
+    setSuccessMessageOpen(false);
+  };
 
   //const inputRef = useRef()
 
   const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0]
+    const selectedFile = e.target.files[0];
 
     if (selectedFile) {
-      setFile(selectedFile)
+      setFile(selectedFile);
 
-      const reader = new FileReader()
+      const reader = new FileReader();
       reader.onloadend = () => {
-        setImagePreview(reader.result)
-      }
-      reader.readAsDataURL(selectedFile)
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(selectedFile);
     }
-  }
+  };
 
   const handleUploadClick = () => {
     // Trigger the input field when the button is clicked to change the photo
     //inputRef.current.click()
-    document.getElementById('profile-picture-input').click()
+    document.getElementById("profile-picture-input").click();
     //console.log('File Uploaded:', file);
 
     //Display the uploaded image
     if (file) {
       //console.log('Profile picture saved:', file)
-      setImagePreview(URL.createObjectURL(file))
+      setImagePreview(URL.createObjectURL(file));
 
       // Reset file state after saving
       //setFile(null)
     }
-  }
+  };
 
   const handleRemoveClick = () => {
-    setFile(null)
-    setImagePreview(null)
-    console.log('Profile Picture Removed')
-  }
+    setFile(null);
+    setImagePreview(null);
+    console.log("Profile Picture Removed");
+  };
 
   const {
     values,
@@ -126,21 +131,21 @@ export default function SystemUser(props) {
     setValues,
   } = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      address: '',
-      email: '',
-      role: '',
-      mobileNo: '',
-      officeNo: '',
-      userName: '',
-      password: '',
-      comment: '',
+      firstName: "",
+      lastName: "",
+      address: "",
+      email: "",
+      role: "",
+      mobileNo: "",
+      officeNo: "",
+      userName: "",
+      password: "",
+      comment: "",
     },
     onSubmit: (values) => {
-      setSuccessMessageOpen(true)
-      setLoading(true)
-      if (props.type === 'add') {
+      setSuccessMessageOpen(true);
+      setLoading(true);
+      if (props.type === "add") {
         systemUserService
           .addSystemUser({
             firstName: values.firstName,
@@ -155,15 +160,15 @@ export default function SystemUser(props) {
             comments: values.comment,
           })
           .then(() => {
-            setLoading(false)
-            navi(AppRoutes.system_user_list.path)
+            setLoading(false);
+            navi(AppRoutes.system_user_list.path);
           })
           .catch((error) => {
-            console.error(error)
-            alert(error)
-            setLoading(false)
-          })
-      } else if (props.type === 'edit') {
+            console.error(error);
+            alert(error);
+            setLoading(false);
+          });
+      } else if (props.type === "edit") {
         systemUserService
           .updateSystemUser(
             {
@@ -181,43 +186,43 @@ export default function SystemUser(props) {
             id
           )
           .then(() => {
-            setLoading(false)
-            navi(AppRoutes.system_user_list.path)
+            setLoading(false);
+            navi(AppRoutes.system_user_list.path);
           })
           .catch((error) => {
-            console.error(error)
-            alert(error)
-            setLoading(false)
-          })
+            console.error(error);
+            alert(error);
+            setLoading(false);
+          });
       }
     },
     validationSchema: SystemUserValidation,
-  })
+  });
 
   useEffect(() => {
-    if (props.type === 'view' || props.type === 'edit') {
-      loadSystemUserData(id, setValues)
+    if (props.type === "view" || props.type === "edit") {
+      loadSystemUserData(id, setValues);
     }
-  }, [id])
+  }, [id]);
 
   useEffect(() => {
-    if (props.type === 'add') {
+    if (props.type === "add") {
       setValues({
-        firstName: '',
-        lastName: '',
-        address: '',
-        email: '',
-        role: '',
-        mobileNo: '',
-        officeNo: '',
-        userName: '',
-        password: '',
-        comment: '',
-      })
+        firstName: "",
+        lastName: "",
+        address: "",
+        email: "",
+        role: "",
+        mobileNo: "",
+        officeNo: "",
+        userName: "",
+        password: "",
+        comment: "",
+      });
     }
-  }, [props.type])
+  }, [props.type]);
 
-  const navi = useNavigate()
+  const navi = useNavigate();
 
   return (
     <Box
@@ -242,7 +247,7 @@ export default function SystemUser(props) {
           <input
             accept="image/*"
             id="profile-picture-input"
-            style={{ display: 'none' }}
+            style={{ display: "none" }}
             type="file"
             onChange={handleFileChange}
           />
@@ -252,7 +257,7 @@ export default function SystemUser(props) {
               fullWidth
               variant="contained"
               onClick={() => {
-                handleUploadClick()
+                handleUploadClick();
               }}
             >
               Add Photo
@@ -263,7 +268,7 @@ export default function SystemUser(props) {
               fullWidth
               variant="contained"
               onClick={() => {
-                handleRemoveClick()
+                handleRemoveClick();
               }}
             >
               Remove
@@ -278,7 +283,7 @@ export default function SystemUser(props) {
         sx={{
           p: 2,
           borderRadius: 3,
-          '& .MuiGrid-item': {
+          "& .MuiGrid-item": {
             padding: 1,
           },
         }}
@@ -297,9 +302,9 @@ export default function SystemUser(props) {
             value={values.firstName} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.firstName && errors.firstName}
-            helperText={touched.firstName ? errors.firstName : ''}
+            helperText={touched.firstName ? errors.firstName : ""}
           />
         </Grid>
 
@@ -316,9 +321,9 @@ export default function SystemUser(props) {
             value={values.lastName} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.lastName && errors.lastName}
-            helperText={touched.lastName ? errors.lastName : ''}
+            helperText={touched.lastName ? errors.lastName : ""}
           />
         </Grid>
 
@@ -336,9 +341,9 @@ export default function SystemUser(props) {
             value={values.address} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.address && errors.address}
-            helperText={touched.address ? errors.address : ''}
+            helperText={touched.address ? errors.address : ""}
           />
         </Grid>
 
@@ -355,9 +360,9 @@ export default function SystemUser(props) {
             value={values.email} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.email && errors.email}
-            helperText={touched.email ? errors.email : ''}
+            helperText={touched.email ? errors.email : ""}
           />
         </Grid>
 
@@ -374,9 +379,9 @@ export default function SystemUser(props) {
             value={values.role} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.role && errors.role}
-            helperText={touched.role ? errors.role : ''}
+            helperText={touched.role ? errors.role : ""}
           >
             {roles.map((option) => (
               <MenuItem key={option.value} value={option.value}>
@@ -399,9 +404,9 @@ export default function SystemUser(props) {
             value={values.mobileNo} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.mobileNo && errors.mobileNo}
-            helperText={touched.mobileNo ? errors.mobileNo : ''}
+            helperText={touched.mobileNo ? errors.mobileNo : ""}
           />
         </Grid>
 
@@ -417,9 +422,9 @@ export default function SystemUser(props) {
             value={values.officeNo} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.officeNo && errors.officeNo}
-            helperText={touched.officeNo ? errors.officeNo : ''}
+            helperText={touched.officeNo ? errors.officeNo : ""}
           />
         </Grid>
 
@@ -436,20 +441,20 @@ export default function SystemUser(props) {
             value={values.userName} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.userName && errors.userName}
-            helperText={touched.userName ? errors.userName : ''}
+            helperText={touched.userName ? errors.userName : ""}
           />
         </Grid>
-        {(props.type === 'add' || props.type == 'edit') && (
+        {(props.type === "add" || props.type == "edit") && (
           <Grid item xs={6}>
             <FormTextField
               required
               id="password"
               name="password"
-              label={props.type == 'edit' ? 'Update password' : 'Password'}
+              label={props.type == "edit" ? "Update password" : "Password"}
               placeholder={
-                props.type == 'edit' ? 'Update password' : 'Enter new password'
+                props.type == "edit" ? "Update password" : "Enter new password"
               }
               variant="filled"
               fullWidth
@@ -457,9 +462,9 @@ export default function SystemUser(props) {
               value={values.password} //set value using formik
               onChange={handleChange} //get onchange value using formik
               onBlur={handleBlur}
-              disabled={props.type === 'view'}
+              disabled={props.type === "view"}
               error={touched.password && errors.password}
-              helperText={touched.password ? errors.password : ''}
+              helperText={touched.password ? errors.password : ""}
             />
           </Grid>
         )}
@@ -477,9 +482,9 @@ export default function SystemUser(props) {
             value={values.comment} //set value using formik
             onChange={handleChange} //get onchange value using formik
             onBlur={handleBlur}
-            disabled={props.type === 'view'}
+            disabled={props.type === "view"}
             error={touched.comment && errors.comment}
-            helperText={touched.comment ? errors.comment : ''}
+            helperText={touched.comment ? errors.comment : ""}
           />
         </Grid>
       </Grid>
@@ -490,12 +495,12 @@ export default function SystemUser(props) {
         pt={3}
         justifyContent="flex-end"
         sx={{
-          '& .MuiButton-root': { m: 1 },
+          "& .MuiButton-root": { m: 1 },
         }}
       >
-        {(props.type === 'add' || props.type === 'edit') && (
+        {(props.type === "add" || props.type === "edit") && (
           <>
-            {' '}
+            {" "}
             <FormClearButton
               variant="outlined
                 "
@@ -515,14 +520,14 @@ export default function SystemUser(props) {
             </FormSaveLoadingButton>
           </>
         )}
-        {props.type === 'view' && (
+        {props.type === "view" && (
           <>
             <FormEditButton
               variant="contained"
               size="large"
               startIcon={<EditIcon />}
               onClick={() =>
-                navi(`${AppRoutes.system_user_edit.path.replace(':id', id)}}`)
+                navi(`${AppRoutes.system_user_edit.path.replace(":id", id)}}`)
               }
             >
               Edit
@@ -535,9 +540,9 @@ export default function SystemUser(props) {
         autoHideDuration={6000}
         onClose={handleCloseSuccessMessage}
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-          marginTop: '64px',
+          vertical: "bottom",
+          horizontal: "center",
+          marginTop: "64px",
         }}
         TransitionComponent={Slide}
       >
@@ -547,18 +552,18 @@ export default function SystemUser(props) {
           onClose={handleCloseSuccessMessage}
           severity="success"
           sx={{
-            fontSize: '1.5rem',
-            textAlign: 'center',
-            display: 'flex',
-            alignItems: 'center',
-            borderRadius: '8px',
+            fontSize: "1.5rem",
+            textAlign: "center",
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "8px",
           }}
         >
-          {props.type === 'add'
-            ? 'Task added successfully!'
-            : 'Task details updated successfully!'}
+          {props.type === "add"
+            ? "Task added successfully!"
+            : "Task details updated successfully!"}
         </MuiAlert>
       </Snackbar>
     </Box>
-  )
+  );
 }
