f28087e2cf0a94b80edd5abf67a2b9240ae0c43c
Punsara Weerasinghe <weerasingha11744@usci.ruh.ac.lk>
2024-04-20 10:38:43 +0530

    Update code line structure in Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -32,91 +32,93 @@ const Home = () => {
   }, []);
 
   return (
-    <Container>
-      <div
-        style={{
-          position: "fixed",
-          marginTop: "-21%",
-          marginLeft: "-5%",
-          width: "100vw",
-          height: "100vh",
-          opacity: "50%",
-        }}
-      >
-        <img
-          src={imagePath}
-          alt="companyLogo"
-          style={{ overflow: "hidden", width: "100%", height: "100%" }}
-        />
-      </div>
+    <>
+      <Container>
+        <div
+          style={{
+            position: "fixed",
+            marginTop: "-21%",
+            marginLeft: "-5%",
+            width: "100vw",
+            height: "100vh",
+            opacity: "50%",
+          }}
+        >
+          <img
+            src={imagePath}
+            alt="companyLogo"
+            style={{ overflow: "hidden", width: "100%", height: "100%" }}
+          />
+        </div>
 
-      <div
-        style={{
-          position: "fixed",
-          marginTop: "-20%",
-          marginLeft: "0%",
-          width: "75vw",
-          height: "100vh",
-          opacity: "100%",
-        }}
-      >
-        <Typography
-          variant="h1"
+        <div
           style={{
-            fontWeight: "bold",
-            fontSize: "75px",
+            position: "fixed",
+            marginTop: "-20%",
+            marginLeft: "0%",
+            width: "75vw",
+            height: "100vh",
+            opacity: "100%",
           }}
         >
-          <p style={{ textAlign: "center", color: "#071024" }}>
-            Welcome to the Rividco
-          </p>
-        </Typography>
-      </div>
+          <Typography
+            variant="h1"
+            style={{
+              fontWeight: "bold",
+              fontSize: "75px",
+            }}
+          >
+            <p style={{ textAlign: "center", color: "#071024" }}>
+              Welcome to the Rividco
+            </p>
+          </Typography>
+        </div>
 
-      <div
-        style={{
-          position: "fixed",
-          marginTop: "-7%",
-          marginLeft: "0%",
-          width: "75vw",
-          height: "100vh",
-          opacity: "80%",
-        }}
-      >
-        <Typography
-          variant="h1"
+        <div
           style={{
-            marginBottom: "5px",
-            fontSize: "60px",
-            fontWeight: "Bold",
-            textAlign: "center",
-            color: "#071024",
+            position: "fixed",
+            marginTop: "-7%",
+            marginLeft: "0%",
+            width: "75vw",
+            height: "100vh",
+            opacity: "80%",
           }}
         >
-          {currentTime.toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: true,
-          })}
-        </Typography>
-        <Typography
-          variant="h1"
-          style={{
-            fontWeight: "bold",
-            textAlign: "center",
-            color: "#071024",
-            fontSize: "30px",
-          }}
-        >
-          {currentTime.toLocaleDateString(undefined, {
-            weekday: "long",
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        </Typography>
-      </div>
-    </Container>
+          <Typography
+            variant="h1"
+            style={{
+              marginBottom: "5px",
+              fontSize: "60px",
+              fontWeight: "Bold",
+              textAlign: "center",
+              color: "#071024",
+            }}
+          >
+            {currentTime.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: true,
+            })}
+          </Typography>
+          <Typography
+            variant="h1"
+            style={{
+              fontWeight: "bold",
+              textAlign: "center",
+              color: "#071024",
+              fontSize: "30px",
+            }}
+          >
+            {currentTime.toLocaleDateString(undefined, {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </Typography>
+        </div>
+      </Container>
+    </>
   );
 };
 
