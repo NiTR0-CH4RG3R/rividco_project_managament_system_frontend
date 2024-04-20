12ab19752057940dff63aaefff1c78d8d34d7acd
sCodeu <sachindu99u@gmail.com>
2024-04-20 22:24:53 +0530

    set validation

--- src/Validation/AddProjectValidation.jsx
+++ src/Validation/AddProjectValidation.jsx
@@ -1,22 +1,29 @@
 import * as yup from "yup";
 
 export const addProjectValidation = yup.object().shape({
-    selectedCustomer: yup.object({
-        title: yup.string().required("Required"),
+    selectedCustomer: yup.object().shape({
+      id: yup.string().required("Required"),
+      firstName: yup.string().required("Required"),
     }),
-
-    selectedReferenceBy: yup.object({
-        title: yup.string().required("Required"),
+  
+    // selectedReferenceBy: yup.object().shape({
+    //   id: yup.string().required("Required"),
+    //   firstName: yup.string().required("Required"),
+    // }),
+  
+    selectedEmployee: yup.object().shape({
+      id: yup.string().required("Required"),
+      firstName: yup.string().required("Required"),
     }),
-
-    selectedEmployee: yup.object({
-        title: yup.string().required("Required"),
-    }),
-    selectedSalesPerson: yup.object({
-        title: yup.string().required("Required"),
-    }),
+  
+    // selectedSalesPerson: yup.object().shape({
+    //   id: yup.string().required("Required"),
+    //   firstName: yup.string().required("Required"),
+    // }),
+  
     description: yup.string().required("Required"),
     status: yup.string().required("Required"),
     location: yup.string().required("Required"),
     startDate: yup.string().required("Required"),
-});
+  });
+  
