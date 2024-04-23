c28c2d454f5951954d4465242177de63cd5b941b
sCodeu <sachindu99u@gmail.com>
2024-04-23 12:32:49 +0530

    add textfield to the commision report

--- src/Pages/Project/CommisionReportModal.jsx
+++ src/Pages/Project/CommisionReportModal.jsx
@@ -1,4 +1,4 @@
-import { Modal, Button } from "@mui/material";
+import { Modal, Button, Grid } from "@mui/material";
 import { Box, Typography } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
@@ -8,6 +8,13 @@ import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { styled } from "@mui/material/styles";
 import { IconButton } from "@mui/material";
 import FormButton from "../../Components/StyledComponents/FormButton";
+import FormTextField from "../../Components/StyledComponents/FormTextField";
+import FormClearButton from "../../Components/StyledComponents/FormClearButton";
+import ClearAllIcon from "@mui/icons-material/ClearAll";
+import SaveIcon from "@mui/icons-material/Save";
+import FormSaveLodingButton from "../../Components/StyledComponents/FormSaveLoadingButton";
+import { useFormik } from "formik";
+import * as yup from 'yup';
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -20,28 +27,37 @@ const VisuallyHiddenInput = styled("input")({
   whiteSpace: "nowrap",
   width: 1,
 });
+const validationSchema = yup.object({
+  description: yup.string().required("Description is required"),
+  reportNumber: yup.string().required("Report number is required"),
+});
 
 export default function CommisionReportModal(props) {
   const { openCommisionReport, setOpenCommisionReport, sendData } = props;
-
-  const customercols = [
-    {
-      field: "fileName",
-      headerName: "File Name",
-      align: "center",
-      headerAlign: "center",
-    },
-  ];
 
   const handleClose = () => {
     setOpenCommisionReport(false);
   };
 
-  const [rows, setRows] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // Fetch data from REST endpoint using axios
-  }, []);
+  const {
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    handleReset,
+    setFieldValue,
+    setValues,
+  } = useFormik({
+    initialValues: {
+      description: "",
+      reportNumber: "",
+    },
+    validationSchema:validationSchema,
+  });
 
   return (
     <Modal open={openCommisionReport} onClose={handleClose}>
@@ -52,7 +68,7 @@ export default function CommisionReportModal(props) {
           left: "50%",
           transform: "translate(-50%, -50%)",
           minWidth: 800,
-          minHeight: 400,
+          //minHeight: 400,
           bgcolor: "background.paper",
           boxShadow: 24,
           p: 4,
@@ -60,32 +76,65 @@ export default function CommisionReportModal(props) {
       >
         <Typography variant="h6">Upload Commision Report</Typography>
         <IconButton
-              aria-label="close"
-              onClick={handleClose}
-              sx={{
-                position: "absolute",
-                right: 8,
-                top: 8,
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
 
-        <div style={{ height: 400, width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={customercols}
-            disableColumnFilter
-            disableColumnSelector
-            disableDensitySelector
-            slots={{ toolbar: GridToolbar }}
-            slotProps={{
-              toolbar: {
-                showQuickFilter: true,
-              },
-            }}
-          />
-        </div>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          onReset={handleReset}
+          noValidate
+          display="flex"
+          //justifyContent="center"
+          //alignItems="center"
+          flexDirection="column"
+          pt={2}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormTextField
+                required
+                placeholder="Please enter the description"
+                id="description"
+                name="description"
+                label="Description"
+                fullWidth
+                size="small"
+                variant="filled"
+                value={values.description}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.description && errors.description}
+                helperText={touched.description ? errors.description : ""}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormTextField
+                required
+                placeholder="Please enter the report number"
+                id="reportNumber"
+                name="reportNumber"
+                label="Report Number"
+                fullWidth
+                size="small"
+                variant="filled"
+                value={values.reportNumber}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.reportNumber && errors.reportNumber}
+                helperText={touched.reportNumber ? errors.reportNumber : ""}
+              />
+            </Grid>
+          </Grid>
+        
 
         <div
           style={{
@@ -108,19 +157,39 @@ export default function CommisionReportModal(props) {
             Upload file
             <VisuallyHiddenInput type="file" />
           </FormButton>
-          {/* <Button
+
+          <FormClearButton
+            variant="outlined"
+            size="large"
+            sx={{
+              mr: 2,
+              width: "8.5rem",
+              margin: "1em 0.5em !important",
+            }}
+            color="primary"
+            startIcon={<ClearAllIcon />}
+            type="reset"
+          >
+            Clear
+          </FormClearButton>
+          <FormSaveLodingButton
+            color="primary"
+            type="submit"
+            size="large"
+            loading={loading}
+            loadingPosition="start"
+            startIcon={<SaveIcon />}
             variant="contained"
             sx={{
               width: "8.5rem",
               margin: "1em 0.5em !important",
             }}
-            color="primary"
-            startIcon={<CloseIcon />}
-            onClick={handleClose}
           >
-            Close
-          </Button> */}
+            <span>Save</span>
+          </FormSaveLodingButton>
+
         </div>
+        </Box>
       </Box>
     </Modal>
   );
