4366f252329aec3e167b2dbe332ac5d5f612eb25
Sayumi De Alwis <sayumidealwis@gmail.com>
2024-04-23 16:12:28 +0530

    added new function to projectservices service

--- src/services/projectServicesService.js
+++ src/services/projectServicesService.js
@@ -2,49 +2,69 @@ import { get, post, put, del } from '../api/axios'
 const SERVICE_URL = '/ProjectService'
 
 export async function listService(projectId, page, itemsPerPage) {
-    const services = [];
+  const services = []
 
-    try {
-        const response = await get(`${SERVICE_URL}`, { projectId, page, pageSize: itemsPerPage });
-        response?.data?.forEach((service) => {
-            services.push(service)
-        })
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await get(`${SERVICE_URL}`, {
+      projectId,
+      page,
+      pageSize: itemsPerPage,
+    })
+    response?.data?.forEach((service) => {
+      services.push(service)
+    })
+  } catch (error) {
+    console.error(error)
+  }
 
-    return services;
+  return services
 }
 
 export async function getService(id) {
-    let service = {};
+  let service = {}
 
-    try {
-        const response = await get(`${SERVICE_URL}/${id}`);
-        service = response?.data;
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await get(`${SERVICE_URL}/${id}`)
+    service = response?.data
+  } catch (error) {
+    console.error(error)
+  }
 
-    return service;
+  return service
 }
 
 export async function addService(service) {
-    try {
-        await post(`${SERVICE_URL}`, service);
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    await post(`${SERVICE_URL}`, service)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export async function updateService(service, id) {
-    try {
-        await put(`${SERVICE_URL}/${id}`, service);
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    await put(`${SERVICE_URL}/${id}`, service)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export async function listProjectServicesByStatus(status, page, itemsPerPage) {
+  const projectServices = []
+
+  try {
+    const response = await get(`${SERVICE_URL}/status`, {
+      status,
+      page,
+      pageSize: itemsPerPage,
+    })
+    response?.data?.forEach((projectService) => {
+      projectServices.push(projectService)
+    })
+    console.log(projectServices)
+  } catch (error) {
+    console.error(error)
+  }
+
+  return projectServices
 }
